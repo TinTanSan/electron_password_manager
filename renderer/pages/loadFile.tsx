d83208b30ee757db2324c9a23f0d553ce3718c0e
TinTanSan <tirthvpatel2003@gmail.com>
2025-07-31 08:59:18 +1000

    added new page to handle opening vault

---
+++ renderer/pages/loadFile.tsx
@@ -0,0 +1,96 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { VaultContext } from '../contexts/vaultContext'
+import { useRouter } from 'next/router'
+import { BannerContext } from '../contexts/bannerContext'
+import { addBanner } from '../interfaces/Banner'
+
+export default function loadFile() {
+    const vaultContext = useContext(VaultContext)
+    const navigate = useRouter()
+    const [recent, setRecent] = useState<Array<string>>([]);
+    const banners = useContext(BannerContext);
+    const handleCreateFile = ()=>{
+        window.ipc.openCreateFile().then((filePath)=>{
+            if (filePath! == ''){
+                // since its a new file, the file content will be empty anyways
+                vaultContext.setVault({filePath, fileContents:""});
+                window.ipc.addRecent(filePath);
+                navigate.push("/home")
+
+            }
+        })
+    }
+
+    const handleOpenFile = (filepath:string | undefined= undefined)=>{
+        // filepath is undefined if the user clicked on open vault, but the filepath will be defined and a valid path
+        // if they picked from the recent vaults
+        if (filepath === undefined){
+            window.ipc.openFilePicker().then((content)=>{
+                if (content !== undefined){
+                    window.ipc.getRecents().then((recents)=>{
+                        setRecent(recents)
+                        vaultContext.setVault(content);
+                        addBanner(banners, "Vault Opened successfully", 'success')
+                    })
+                }else{
+                    addBanner(banners, "No vault chosen", 'warning')
+                }
+                
+
+            })
+        }else{
+            window.ipc.openFile(filepath).then((content)=>{
+                if (content !== undefined){
+                    vaultContext.setVault(content);
+                    console.log("content set")
+                    addBanner(banners, "Vault Opened successfully", 'success')
+                }else{
+                    // this is different to the undefined in the above if branch because when we directly call openFile we are
+                    // using an filesystem read to open the filepath, and undefined means something went wrong finding that file
+                    addBanner(banners, "Vault Not found", 'error')
+                    vaultContext.setVault(undefined);
+                }
+            });
+        }
+        
+    }
+
+    useEffect(()=>{
+        window.ipc.getRecents().then((x)=>{setRecent(x)})
+    },[])
+
+  return (
+    <div className='grid w-screen h-screen grid-flow-row p-5 grid-rows-4 bg-base-200 gap-20 text-base-content'>
+        
+        {/* recently opened files */}
+        <div className='grid grid-flow-row-dense row-span-2 grid-rows-10 grid-cols-1 justify-center bg-base-100 rounded-lg border-2 border-base-300'>
+            <div className='flex w-full row-span-1 h-full items-center text-xl justify-center row-start-1'>Recently opened vaults</div>
+            <div className='grid grid-flow-row-dense grid-rows-10 grid-cols-1 row-span-full row-start-2 w-full gap-2 overflow-y-auto p-2'>
+                {
+                    recent.map((x,i)=>(
+                        <button onClick={()=>{handleOpenFile(x)}} key={i} className='grid cursor-pointer row-span-1 rounded-md px-5 h-fit hover:bg-base-300 bg-base-200 w-full text-ellipsis'>
+                            {x.substring(1)}
+                        </button>
+                    ))
+                }
+            </div>
+        </div>
+        
+        <div className="grid grid-flow-row-dense gap-5 row-span-1 justify-center">
+            <div className='grid grid-flow-row-dense row-span-1 justify-center text-wrap shrink flex-wrap'>
+                To load an existing vault click Load vaul, otherwise create a new vault using the create vault button.
+            </div>
+            <div className='grid grid-flow-row-dense lg:grid-flow-col-dense  gap-5 row-span-1 justify-center'>
+                <button onClick={()=>{handleOpenFile()}} className='flex justify-center items-center bg-primary hover:bg-primary-darken text-primary-content w-32 h-10 rounded-lg transition-all duration-700 hover:rounded-xl'>
+                    load Vault
+                </button>
+                <button onClick={()=>{handleCreateFile()}} className='flex justify-center items-center bg-primary hover:bg-primary-darken text-primary-content w-32 h-10 rounded-lg transition-all duration-700 hover:rounded-xl'>
+                    Create Vault
+                </button>
+            </div>
+        </div>
+        
+        
+    </div>
+  )
+}
