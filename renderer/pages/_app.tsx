1952241a79e08ecbe6ea24f749cd50e75199f5a9
TinTanSan <tirthvpatel2003@gmail.com>
2025-07-31 08:58:42 +1000

    now using vaultcontext and bannercontext to handle vault and notifications

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
+import { VaultContext } from '../contexts/vaultContext'
+import { BannerContext } from '../contexts/bannerContext'
+import { BannerDetails } from '../interfaces/Banner'
+import Notifications from '../components/notifications'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [vault, setVault] = useState()
+  const [banners, setBanners] = useState<Array<BannerDetails>>([]);
+  return (
+    <VaultContext.Provider value={{vault, setVault}}>
+      <BannerContext.Provider value={{banners, setBanners}}>
+        <Notifications />
+        <Component {...pageProps} />
+      </BannerContext.Provider>
+    </VaultContext.Provider>
+  )
 }
 
 export default MyApp
