b0d05fdad9ab581850b23ccea23ed3497d78c7e9
TinTanSan <tirthvpatel2003@gmail.com>
2025-07-31 08:57:58 +1000

    removed the close all div entirely

--- renderer/components/notifications.tsx
+++ renderer/components/notifications.tsx
@@ -5,17 +5,9 @@ import Banner from './banner';
 // this one hosts all the banners from the banner context
 export default function Notifications() {
   const banners = useContext(BannerContext);
-  useEffect(()=>{
-    if (banners.banners.length> 0){
-
-    }
-  },[banners.banners])
 
   return (
-    <div className='flex flex-col gap-5 z-50 w-[30vw] h-fit fixed top-0 right-0'>
-      {banners.banners.length > 0 && <div>
-          <button className='flex'>Close all</button>
-        </div>}
+    <div className='flex flex-col gap-2 z-50 w-[30vw] h-fit fixed top-0 right-0 p-2 overflow-y-scroll'>
       {
         banners.banners.map((x,i)=>(
           <Banner bannerDetails={x} key={i} />
