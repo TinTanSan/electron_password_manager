9d04a32283adfd1c505599d4fdb3a79bb8e41571
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-01 13:51:36 +1000

    using the unlockVault component

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,13 +1,29 @@
 // 'use client'
-import React, { useEffect, useState } from 'react'
-import { createEntry, decryptEntryPass } from '../utils/entryFunctions';
+import React, { useContext, useEffect, useState } from 'react'
+import { VaultContext } from '../contexts/vaultContext';
+import { useRouter } from 'next/router';
+import { BannerContext } from '../contexts/bannerContext';
+import UnlockVaultPrompt from '../components/unlockVaultPrompt';
+
 
 export default function HomePage() {
-  const handleClick = ()=>{
-  }
+  const vaultContext = useContext(VaultContext);
+  const bannerContext = useContext(BannerContext);
+  // const naviagte = useRouter();
+  
+  // whether or not the vault is unlocked, automatically lock after 1 minute of inactivity
+  useEffect(()=>{
+    vaultContext.setVault(({
+      fileContents:"", 
+      filePath:"/Users/t/Desktop/coding/web_dev/password_manager/test.vlt", 
+      isUnlocked:false, 
+      kek:undefined
+    }))
+  },[])
+
   return (
-    <div className='flex bg-base-200 w-screen h-screen'>
-      <button onClick={()=>{handleClick()}} className='border-2 bg-base-300 h-10 w-30 rounded-lg'>Click me</button>
+    <div className='flex bg-base-200 w-screen h-screen flex-col justify-center items-center'>
+      {(vaultContext.vault !== undefined && !vaultContext.vault.isUnlocked) && <UnlockVaultPrompt />}
     </div>
   )
 }
