7b5df1064bbd0ed2e56ebace0788a9bd5fd576c9
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-01 13:51:52 +1000

    added extra properties to vaultType

--- renderer/contexts/vaultContext.tsx
+++ renderer/contexts/vaultContext.tsx
@@ -1,7 +1,13 @@
 import { createContext } from "react";
 
-type VaultType = {filePath:string, fileContents:string}
+export type VaultType = {
+    filePath:string, 
+    fileContents:string
+    isUnlocked:boolean,
+    kek:KEKParts | undefined, //KEK should be set to undefined when the vault is locked 
+}
 
-type VaultCxtType = {vault: VaultType, setVault: React.Dispatch<React.SetStateAction<VaultType>>}
+type VaultCxtType = {
+    vault: VaultType, setVault: React.Dispatch<React.SetStateAction<VaultType>>}
 
 export const VaultContext = createContext<VaultCxtType | undefined>(undefined)
