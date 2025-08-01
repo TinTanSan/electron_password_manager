f25828db6cece26732aae38e147d59869fca6d46
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-01 13:51:18 +1000

    added a useeffect overwatching vault which can be used later to commit changes to the actual vault file

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
-import { VaultContext } from '../contexts/vaultContext'
+import { VaultContext, VaultType } from '../contexts/vaultContext'
 import { BannerContext } from '../contexts/bannerContext'
 import { BannerDetails } from '../interfaces/Banner'
 import Notifications from '../components/notifications'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [vault, setVault] = useState()
+  const [vault, setVault] = useState<VaultType>()
   const [banners, setBanners] = useState<Array<BannerDetails>>([]);
+  useEffect(()=>{
+    console.log("vault contents changed", vault)
+  }, [vault])
   return (
     <VaultContext.Provider value={{vault, setVault}}>
       <BannerContext.Provider value={{banners, setBanners}}>
