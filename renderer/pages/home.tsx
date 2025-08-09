41aaa6f64213f3fcd567f23b48986e76b97f8fbb
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-09 15:27:33 +1000

    removed useEffect which was overwriting vault context

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -9,21 +9,19 @@ import UnlockVaultPrompt from '../components/unlockVaultPrompt';
 export default function HomePage() {
   const vaultContext = useContext(VaultContext);
   const bannerContext = useContext(BannerContext);
-  // const naviagte = useRouter();
+  const naviagte = useRouter();
   
   // whether or not the vault is unlocked, automatically lock after 1 minute of inactivity
-  useEffect(()=>{
-    vaultContext.setVault(({
-      fileContents:"", 
-      filePath:"/Users/t/Desktop/coding/web_dev/password_manager/test.vlt", 
-      isUnlocked:false, 
-      kek:undefined
-    }))
-  },[])
 
   return (
     <div className='flex bg-base-200 w-screen h-screen flex-col justify-center items-center'>
       {(vaultContext.vault !== undefined && !vaultContext.vault.isUnlocked) && <UnlockVaultPrompt />}
+      { (vaultContext.vault !== undefined && vaultContext.vault.isUnlocked) && 
+        <div>
+          Home
+
+        </div>
+      }
     </div>
   )
 }
