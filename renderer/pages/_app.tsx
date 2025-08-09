f478fde86cc0914489bb4c75e3747d793ba88268
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-09 15:25:44 +1000

    now re-routing to the loadFile page when vault is undefined

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -6,12 +6,16 @@ import { VaultContext, VaultType } from '../contexts/vaultContext'
 import { BannerContext } from '../contexts/bannerContext'
 import { BannerDetails } from '../interfaces/Banner'
 import Notifications from '../components/notifications'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [vault, setVault] = useState<VaultType>()
   const [banners, setBanners] = useState<Array<BannerDetails>>([]);
+  const navigate = useRouter();
   useEffect(()=>{
     console.log("vault contents changed", vault)
+    if(vault === undefined){navigate.push('loadFile')
+    }
   }, [vault])
   return (
     <VaultContext.Provider value={{vault, setVault}}>
