43a9628a17489868fb8bf782149dc0609ae06fdb
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-09 15:26:32 +1000

    limiting banners to only 5 most recent banners

--- renderer/components/notifications.tsx
+++ renderer/components/notifications.tsx
@@ -5,9 +5,14 @@ import Banner from './banner';
 // this one hosts all the banners from the banner context
 export default function Notifications() {
   const banners = useContext(BannerContext);
+  useEffect(()=>{
+    if (banners.banners.length > 5){
+      banners.setBanners(banners=>banners.toSpliced(5,banners.length-5))
+    }
 
+  },[banners.banners])
   return (
-    <div className='flex flex-col gap-5 z-50 w-[30vw] h-fit fixed top-0 right-0 p-2 overflow-y-scroll'>
+    <div className='flex flex-col gap-5 z-50 w-[30vw] h-fit fixed top-0 right-0 p-2 overflow-y-scroll overflow-x-hidden'>
       {
         banners.banners.map((x,i)=>(
           <Banner bannerDetails={x} key={i} />
