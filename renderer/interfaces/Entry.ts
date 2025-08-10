353487674c7f257b5252faf24ee784fdddd4bc00
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-10 10:46:37 +1000

    testing buffer instead of string to allow for 1:1 storage

--- renderer/interfaces/Entry.ts
+++ renderer/interfaces/Entry.ts
@@ -1,8 +1,8 @@
 export interface Entry{
-    dek: CryptoKey,
+    dek: Buffer,
     title    : string,
     username : string,
-    password : string,
+    password : Buffer,
     notes    : string //notes field is optional for user to enter, but otherwise it will be an empty string 
     metadata : MetaData
 }
