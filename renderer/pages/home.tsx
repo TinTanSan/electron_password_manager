cff401172a471d5bd69ba9af70a68e504e321a60
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-10 10:47:01 +1000

    testing buffer based entry fields

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -4,22 +4,47 @@ import { VaultContext } from '../contexts/vaultContext';
 import { useRouter } from 'next/router';
 import { BannerContext } from '../contexts/bannerContext';
 import UnlockVaultPrompt from '../components/unlockVaultPrompt';
+import Navbar from '../components/Navbar';
+import { Entry } from '../interfaces/Entry';
+import { createEntry, decryptEntryPass } from '../utils/entryFunctions';
 
 
 export default function HomePage() {
   const vaultContext = useContext(VaultContext);
   const bannerContext = useContext(BannerContext);
   const naviagte = useRouter();
-  
+  const [entries, setEntries] = useState<Array<Entry>>([]);
+  useEffect(()=>{
+    if(vaultContext.vault !== undefined && vaultContext.vault.kek !== undefined){
+      createEntry('a', 'b',' c','',vaultContext.vault.kek).then((entry)=>{
+        console.log(entry.password)
+        decryptEntryPass(entry, vaultContext.vault.kek).then((x)=>{
+          console.log(x)
+        })
+      })
+      // vaultContext.vault.fileContents.split("\n");
+    }
+  },[vaultContext])
   // whether or not the vault is unlocked, automatically lock after 1 minute of inactivity
 
   return (
-    <div className='flex bg-base-200 w-screen h-screen flex-col justify-center items-center'>
+    <div className='flex bg-base-200 w-screen h-screen flex-col justify-center items-center p-2'>
       {(vaultContext.vault !== undefined && !vaultContext.vault.isUnlocked) && <UnlockVaultPrompt />}
       { (vaultContext.vault !== undefined && vaultContext.vault.isUnlocked) && 
-        <div>
-          Home
-
+        <div className='flex flex-col w-full h-full items-center gap-2'>
+          <Navbar />
+          <div className='flex w-full h-full overflow-y-auto gap-y-5  transition-all duration-700 gap-5 px-2 rounded-lg flex-wrap'>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+            <div className='bg-base-100 basis-md shrink-0 max-w-lg border-2 border-base-300 shadow-lg w-full h-90 rounded-xl'/>
+          </div>
         </div>
       }
     </div>
