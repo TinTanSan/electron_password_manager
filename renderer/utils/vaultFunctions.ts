c1976f0815ece6a10085e7ebc2be8ae627a94a56
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-11 13:31:06 +1000

    major updates to vault level encrypt and decrypt, both of which are functional

--- renderer/utils/vaultFunctions.ts
+++ renderer/utils/vaultFunctions.ts
@@ -4,55 +4,115 @@
 import { error } from "console";
 import { Entry } from "../interfaces/Entry";
 import { makeNewDEK } from "./keyFunctions";
+import { decryptEntryPass } from "./entryFunctions";
 
 // this function is to do with commiting a KEK whenever the master password changes, it will commit a salt,
 // and a sanity check value to the first line of the vault file
-export async function commitKEK(filePath:string,fileContents:string,kek:KEKParts){
+export async function commitKEK(filePath:string,fileContents:Buffer,kek:KEKParts){
     // remove the first line, which contains important content retaining to the KEK
-    const content = fileContents.split("\n").splice(0,1);
-    let firstLine = kek.salt + "|"+kek.digest + "|";
+    const content = fileContents.subarray(48);
+    console.log(content.length)
     if (typeof window !== "undefined"){
-        const dek = await makeNewDEK()
-        window.crypto.subtle.wrapKey('raw', dek, kek.kek, {name:'AES-KW'});
-
-        firstLine += Buffer.from(await window.crypto.subtle.exportKey("raw", dek)).toString('base64')
-        window.ipc.writeFile(filePath,firstLine+"\n"+content);
+        const VK = await makeNewDEK()
+        const wrappedVK = Buffer.from(await window.crypto.subtle.wrapKey('raw', VK, kek.kek, {name:'AES-KW'}));
+        const allContent = Buffer.concat([kek.salt,wrappedVK,content]);
+        window.ipc.writeFile(filePath,allContent);
+        return wrappedVK
     }else{
-        throw new Error('window object was underfined')
+        throw new Error('window object was undefined')
     }
 }
 
-export async function vaultLevelEncrypt(entries:Array<Entry>, wrappedVK:string, kek:KEKParts ){
+export async function vaultLevelEncrypt(entries:Array<Entry>, wrappedVK:Buffer, kek:KEKParts){
     if (typeof window !=="undefined"){
         const crypto = window.crypto.subtle;
-        const VK = await crypto.unwrapKey('raw', Buffer.from(wrappedVK, 'base64'), kek.kek,{name:"AES-KW"},{name:"AES-GCM"},false, ['encrypt', 'decrypt'])
+        const VK = await crypto.unwrapKey(
+            'raw', 
+            Buffer.from(wrappedVK), 
+            kek.kek,
+            {name:"AES-KW"},
+            {name:"AES-GCM"},
+            false, 
+            ['encrypt', 'decrypt']
+        )
         const iv = new Uint8Array(12);
+        
         window.crypto.getRandomValues(iv);
-        crypto.encrypt({
-            name:"AES-GCM",
-            iv: Buffer.from(iv),
-
-        }, VK, Buffer.from("entries",'ascii'))
+        const content = entries.map((x)=>
+            x.title+"|" + 
+            x.username + "|" + 
+            x.dek.toString('base64')+ "|" +
+            x.password.toString('base64')+ "|" + //we base64 encode the DEK and password because they can possibly contain the '|' symbol which would
+                                                 //  improperly delimit the encrypted text, which would have serious implications when decrypting
+             x.notes + "|"+ 
+            x.metadata.createDate.toISOString()+ "|" + 
+            x.metadata.lastEditedDate.toISOString()).join("$") 
+        + "$"; //the + "$" is to ensure that we wrap the end by a $ so that even if there is only 1 entry, there will be at least one $ symbol
+        
+        const enc = Buffer.concat([
+                kek.salt,
+                wrappedVK,
+                Buffer.from(await crypto.encrypt({name:"AES-GCM", iv: Buffer.from(iv)},  VK,  Buffer.from(content))), // actual ciphertext
+                iv //  associated iv
+        ]);
+        console.log(enc)
+        return enc
     }
 }
 
-export async function vaultLevelDecrypt(fileContents:string, kek:KEKParts){
-    const [_salt,_digest,vk_raw] = fileContents.split("\n")[0].split("|")
-    const entries = fileContents.substring(fileContents.indexOf("\n") + 1);
-    const iv = entries.substring(entries.lastIndexOf("\n")+1);
+export async function writeEntriesToFile(entries:Array<Entry>, filePath:string, wrappedVK:Buffer, kek:KEKParts){
+    if (typeof window !=="undefined"){
+        const content = Buffer.concat([kek.salt,wrappedVK, Buffer.from(await vaultLevelEncrypt(entries, wrappedVK,kek))]);
+        const result = await window.ipc.writeFile(filePath, content);
+        return result === "OK" ? {content, status:result} : {content:undefined, status:result};
+    }
+    
+}
+
+export async function vaultLevelDecrypt(fileContents:Buffer, kek:KEKParts){
+    const wrappedVK = fileContents.subarray(16,56);
+    const iv = fileContents.subarray(fileContents.length-12);
     if (typeof window !== "undefined"){
-        const vk_bytes = Buffer.from(vk_raw, 'base64');
         const vk = await window.crypto.subtle.unwrapKey(
             'raw',
-            vk_bytes,
+            Buffer.from(wrappedVK),
             kek.kek, 
             {name:"AES-KW"},
             {name:"AES-GCM"}, 
             false, 
             ['encrypt', 'decrypt']
         );
+        const encContents = Buffer.from(fileContents.subarray(56,fileContents.length-12));
+        const decryptedItems = Buffer.from(await window.crypto.subtle.decrypt({name:"AES-GCM", iv:Buffer.from(iv)},vk, encContents));
+        let entries_raw = [];
+        let curEntry = [];
+        for (let i = 0; i<decryptedItems.length; i++){
+            if(decryptedItems[i] !== 0x24){
+                curEntry.push(decryptedItems[i]);
+            }else{
+                entries_raw.push(curEntry);
+                curEntry = [];
+            }
+        }
+        const entries = entries_raw.map((x)=>{
+            const [title, username,dek, password, notes, createDate, lastEditedDate] = Buffer.from(x).toString('utf8').split("|");
+            const entry:Entry = {
+                title,
+                username,
+                dek:Buffer.from(dek, 'base64'),
+                password: Buffer.from(password, 'base64'),
+                notes,
+                metadata:{
+                    createDate:new Date(createDate),
+                    lastEditedDate:new Date(lastEditedDate)
+                }
+            }
+            return entry
+        });
 
-        window.crypto.subtle.decrypt({name:"AES-GCM", iv:Buffer.from(iv, 'base64')},vk, Buffer.from(entries, 'base64'))
+
+
+        return entries
     }
     throw new Error("Window object was undefined")
 }
