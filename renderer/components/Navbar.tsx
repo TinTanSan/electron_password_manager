ab0632ef60da2b4c815a0053201b721c89d083c2
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-11 13:27:10 +1000

    simple navbar styling

--- renderer/components/Navbar.tsx
+++ renderer/components/Navbar.tsx
@@ -2,8 +2,14 @@ import React, { useContext } from 'react'
 import { VaultContext } from '../contexts/vaultContext'
 import { BannerContext } from '../contexts/bannerContext';
 import { addBanner } from '../interfaces/Banner';
+import FancyInput from './fancyInput';
 
-export default function Navbar() {
+type props = {
+    search:string,
+    setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Navbar({search, setSearch}:props) {
     const vaultContext = useContext(VaultContext);
     const bannerContext = useContext(BannerContext);
 
@@ -19,11 +25,14 @@ export default function Navbar() {
 
 
     return (
-        <div className='flex w-full h-10 flex-row items-center justify-between px-2 shadow-lg bg-base-100 border-2 border-base-300 rounded-lg'>
-            <button className='flex bg-primary hover:bg-primary-darken text-primary-content w-24 h-8 justify-center items-center rounded-lg'>New</button>
+        <div className='flex w-full h-12 relative flex-row items-center justify-between px-1 shadow-lg bg-base-100 border-2 border-base-300 rounded-xl'>
+            <button className='flex bg-primary hover:bg-primary-darken text-primary-content w-32 shrink-0 h-8 justify-center items-center rounded-lg text-nowrap'>New Entry</button>
+            <div className='flex justify-center w-full px-5 text-xl shrink grow'>
+                <FancyInput value={search} setValue={setSearch} placeHolder='search for an entry' type='text'  />
+            </div>
             <div className='flex w-fit gap-2 ' >
-                <button onClick={handleLock} className='flex bg-accent hover:bg-accent-darken text-accent-content w-24 justify-center items-center rounded-lg h-8' title='locking will send you back to the unlock page'>Lock</button>
-                <button onClick={handleClose} className='flex bg-primary hover:bg-primary-darken text-primary-content w-24 justify-center items-center rounded-lg h-8'  title='closing will allow you to open anohter vault'>Close Vault</button>
+                <button onClick={handleLock} className='flex bg-accent hover:bg-accent-darken shrink-0 text-accent-content w-24 justify-center items-center rounded-lg h-8' title='locking will send you back to the unlock page'>Lock</button>
+                <button onClick={handleClose} className='flex bg-primary hover:bg-primary-darken shrink-0 text-primary-content w-24 justify-center items-center rounded-lg h-8'  title='closing will allow you to open anohter vault'>Close Vault</button>
             </div>
         </div>
   )
