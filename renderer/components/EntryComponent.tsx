88a879fe0336006fff3f4dbd2db1edfa90fde363
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-19 11:55:51 +1000

    added functions to show and copy password from entry component

--- renderer/components/EntryComponent.tsx
+++ renderer/components/EntryComponent.tsx
@@ -1,27 +1,64 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Entry } from '../interfaces/Entry'
+import EditEntryModal from './editEntryModal';
+import Image from 'next/image';
+import { decryptEntryPass } from '../utils/entryFunctions';
+import { VaultContext } from '../contexts/vaultContext';
+import { addBanner } from '../interfaces/Banner';
+import { BannerContext } from '../contexts/bannerContext';
 type props={
     entry: Entry
 }
 
 export default function EntryComponent({entry}:props) {
-    
-
+    const [showEditModal, setShowEditModal] = useState(false);
+    const {vault, setVault} = useContext(VaultContext);
+    const bannerContext = useContext(BannerContext);
+    const [decryptedPass, setDecryptedPass] = useState<string | undefined>(undefined);
+    const handleShowPass = ()=>{
+        decryptedPass === undefined?
+        decryptEntryPass(entry, vault.kek).then((pass)=>{
+            setDecryptedPass(pass);
+            setTimeout(() => {
+                setDecryptedPass(undefined);
+            }, 10000);
+        })
+        :
+        setDecryptedPass(undefined);
+    }
+    const handleCopy = ()=>{
+        decryptEntryPass(entry, vault.kek).then((pass)=>{
+            navigator.clipboard.writeText(pass).then(()=>{
+                // set clipboard to be empty after 10 seconds
+                addBanner(bannerContext, 'password copied to clipboard', 'success')
+                setTimeout(() => {
+                    window.ipc.clearClipboard();
+                    console.log('clear called')
+                    // navigator.clipboard.writeText("").catch((reason)=>{
+                    //     addBanner(bannerContext, 'unable to clear item from clipboard '+reason, 'error');
+                    // })
+                }, 2000);
+            })
+        })
+    }
   return (
     <div className='flex relative flex-col basis-md items-center grow max-w-md h-90 gap-2 shrink-0 border-2 border-base-300 bg-base-100 shadow-xl/20 w-full  rounded-xl p-2'>
+        {showEditModal && <EditEntryModal uuid={entry.metadata.uuid} setShowModal={setShowEditModal}/>}
         <div className="justify-center flex w-full font-bold text-xl">{entry.title}</div>
         <div className="flex flex-col w-full h-full gap-2 px-2">        
             <div className='flex w-full h-10 gap-2'>
                 <div>Username:</div>
                 <div>{entry.username}</div>
             </div>
-            <div className='flex w-full h-10 gap-2'>
-                <div>Password:</div>
-                <div>{'*'.repeat(8)}</div>
+            <div className='flex w-full h-10 gap-2 items-center'>
+                <div className='flex'>Password:</div>
+                <div className='flex w-full rounded-md px-1 pt-1 items-end border-2 align-text-bottom'>{decryptedPass===undefined?'*'.repeat(Math.max(8,Math.floor(Math.random()*15))): decryptedPass}</div>
+                <Image src={decryptedPass===undefined?"/images/showPass.svg":"/images/hidePass.svg"} alt={decryptedPass===undefined?"show":"hide"} width={20} height={30} className='flex h-auto cursor-pointer' onClick={()=>{handleShowPass()}} />
+                <Image onClick={()=>{handleCopy()}} src={"/images/copy.svg"} alt='copy' height={20} width={20} className='flex w-auto cursor-pointer' />
             </div>
             <div className='flex h-full rounded-lg p-1 border-2 shrink w-full'>{entry.notes}</div>
         </div>
-        <button className='flex w-1/2 rounded-lg justify-center h-10 items-center bg-primary hover:bg-primary-darken text-primary-content'>Edit Entry</button>
+        <button onClick={()=>{setShowEditModal(true)}} className='flex w-1/2 rounded-lg justify-center h-10 items-center bg-primary hover:bg-primary-darken text-primary-content'>Edit Entry</button>
     </div>
   )
 }
