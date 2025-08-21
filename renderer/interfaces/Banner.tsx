f4a1e7f5e5571aaeb0bd8cff85bffca07325cfb3
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-21 11:17:23 +1000

    added logic to remove banner 3 seconds after it is added to the banner context as well as added logic to clip the banners to only 5 banners

--- renderer/interfaces/Banner.tsx
+++ renderer/interfaces/Banner.tsx
@@ -2,14 +2,23 @@ import {BannerCTXType} from '../contexts/bannerContext';
 
 
 export interface BannerDetails{
-    id: number;
+    id: string;
     text: string,
-    type: 'info' |'success' | 'warning' | 'error' ,// we have four types of banners 
+    type: 'info' |'success' | 'warning' | 'error' ,// we have four types of banners ,
 }
 // simple function to abstract away the adding of banners into the context. 
 export function addBanner(bannerContext:BannerCTXType | undefined, text:string, type:'info' |'success' | 'warning' | 'error' ){
     if (bannerContext !== undefined){
-        bannerContext.setBanners(prev=>[...prev, {id:Date.now(), text, type}])
+        const id = window.crypto.randomUUID();
+        bannerContext.setBanners(prev => {
+            const updated = [...prev, {id, text, type}];
+            return updated.length > 5 ? updated.slice(-5) : updated;
+        });
+  
+        setTimeout(() => {
+            bannerContext.setBanners(prev=>prev.filter(x=>x.id !== id))
+        }, 3000);
+
     }else{
         throw new Error("Tried to add a notification to the banners, but the bannerContext was undefined.")
     }
