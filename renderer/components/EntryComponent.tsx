144ad2b9047148c4fd1ba9032a7eb9886ec9d248
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-22 18:51:24 +1000

    renamed file

--- renderer/components/EntryComponent.tsx
+++ renderer/components/EntryComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Entry } from '../interfaces/Entry'
-import EditEntryModal from './editEntryModal';
+import EntryModal from './EntryModal';
 import Image from 'next/image';
 import { VaultContext } from '../contexts/vaultContext';
 import { addBanner } from '../interfaces/Banner';
@@ -44,7 +44,7 @@ export default function EntryComponent({entry}:props) {
     }   
   return (
     <div className='flex relative flex-col basis-md items-center grow max-w-md h-90 gap-2 shrink-0 border-2 border-base-300 bg-base-100 shadow-xl/20 w-full  rounded-xl p-2'>
-        {showEditModal && <EditEntryModal uuid={entry.metadata.uuid} setShowModal={setShowEditModal}/>}
+        {showEditModal && <EntryModal uuid={entry.metadata.uuid} setShowModal={setShowEditModal}/>}
         <div className="justify-center flex w-full font-bold text-xl">{entry.title}</div>
         <div className="flex flex-col w-full h-full gap-2 px-2">        
             <div className='flex w-full h-10 gap-2'>
@@ -53,7 +53,7 @@ export default function EntryComponent({entry}:props) {
             </div>
             <div className='flex w-full h-10 gap-2 items-center'>
                 <div className='flex'>Password:</div>
-                <div className='flex w-full rounded-md px-1 pt-1 items-end border-2 align-text-bottom overflow-x-auto'>{decryptedPass===undefined?'*'.repeat(Math.max(8,Math.floor(Math.random()*15))): decryptedPass}</div>
+                <div className='flex w-full rounded-md px-1 pt-1 items-end border-2 align-text-bottom overflow-x-auto h-8'>{decryptedPass===undefined?'*'.repeat(Math.max(8,Math.floor(Math.random()*15))): decryptedPass}</div>
                 <Image src={decryptedPass===undefined?"/images/showPass.svg":"/images/hidePass.svg"} alt={decryptedPass===undefined?"show":"hide"} width={25} height={25} className='flex w-auto cursor-pointer' onClick={()=>{handleShowPass()}} />
                 <Image onClick={()=>{handleCopy()}} src={"/images/copy.svg"} alt='copy' height={20} width={20} className='flex h-auto cursor-pointer' />
             </div>
@@ -61,7 +61,7 @@ export default function EntryComponent({entry}:props) {
         </div>
         <div className='flex w-full gap-2 px-2'>
             <button type='button' onClick={handleDelete} className='flex w-1/3 items-center justify-center rounded-lg bg-error text-error-content hover:bg-error-darken hover:text-white'>Delete</button>
-            <button onClick={()=>{setShowEditModal(true)}} className='flex w-full rounded-lg justify-center h-10 items-center bg-primary hover:bg-primary-darken text-primary-content'>Edit Entry</button>
+            <button onClick={()=>{setShowEditModal(true)}} className='flex w-full rounded-lg justify-center h-10 items-center bg-primary hover:bg-primary-darken text-primary-content'>Details &amp; Edit</button>
             
         </div>
     </div>
