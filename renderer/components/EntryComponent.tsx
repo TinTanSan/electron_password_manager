bf89b42e988a3d06dae56174f6f682eeef3d0a50
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-30 09:28:34 +1000

    added clickable images for copy and hide/show password

--- renderer/components/EntryComponent.tsx
+++ renderer/components/EntryComponent.tsx
@@ -45,12 +45,18 @@ export default function EntryComponent({entry}:props) {
     <div className='flex w-full odd:bg-base-200 even:bg-base-300 rounded-lg text-base-content h-14 py-2'>
         <div className='flex w-full h-full items-center border-r-2 border-base-100 justify-center'>{entry.title? entry.title: <i>No Title</i>}</div>
         <div className='flex w-full h-full items-center border-r-2 border-base-100 justify-center'>{entry.username? entry.username: <i>No username</i>}</div>
-        <div className='flex w-full h-full items-center border-r-2 border-base-100 justify-center'>{decryptedPass? decryptedPass? decryptedPass:<i>No Password</i> : "*".repeat(Math.max(8, Math.random()*15))}</div>
+        <div className='flex w-full h-full items-center border-r-2 border-base-100 justify-center'>
+            <div className='flex w-full h-full border-2'>
+                {decryptedPass? decryptedPass? decryptedPass:<i>No Password</i> : "*".repeat(Math.max(8, Math.random()*15))}
+            </div>
+            <Image src={'/images/copy.svg'} alt='copy' width={25} height={25} className='w-auto h-auto flex items-center justify-center' />
+            <Image onClick={()=>{handleShowPass()}} src={decryptedPass?'/images/hidePass.svg':'/images/showPass.svg'} alt={decryptedPass?'hide':'show'} width={15} height={15} className='w-auto h-auto flex items-center justify-center' />
+        </div>
         <div className='flex w-full h-full items-center justify-center text-nowrap text-ellipsis overflow-hidden'>{entry.notes ? entry.notes :<i>No Notes</i>}</div>
         {/* details/edit and delete buttons */}
         <div className='flex w-full h-full items-center gap-2'>
-            <button type='button' className='flex w-full h-full  items-center justify-center border-2 rounded-lg bg-primary text-primary-content'>Details/Edit</button>
-            <button type='button' className='flex w-10 justify-center rounded-lg h-full items-center bg-error'><Image src={'/images/delete.svg'} alt='del' width={25} height={25} className='flex w-auto bg-error'/></button>
+            <button onClick={()=>{setShowEditModal(prev=>!prev)}} type='button' className='flex w-full h-full  items-center justify-center border-2 rounded-lg bg-primary text-primary-content'>Details/Edit</button>
+            <button onClick={()=>{handleDelete()}} type='button' className='flex w-10 justify-center rounded-lg h-full items-center bg-error'><Image src={'/images/delete.svg'} alt='del' width={25} height={25} className='flex w-auto bg-error'/></button>
         </div>
     </div>
   )
