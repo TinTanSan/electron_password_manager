1e57e147f900369604b845fc2060c55fc6a8bb48
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-03 15:32:56 +1000

    made the section selectors look more like tab selectors

--- renderer/components/NewEntryForm.tsx
+++ renderer/components/NewEntryForm.tsx
@@ -85,48 +85,47 @@ export default function NewEntryForm({setShowForm}:props) {
   
     return (
         <div className='backdrop-blur-lg z-20 fixed w-screen h-screen top-0 left-0 flex flex-col justify-center items-center'>
-            <form onSubmit={handleAdd} className='flex flex-col relative w-1/2 h-[80vh] border-2 border-base-300 bg-base-100 z-10 shadow-lg rounded-xl p-2 px-5 items-center gap-2'>
+            <form onSubmit={handleAdd} className='flex flex-col relative w-1/2 h-[80vh] border-2 border-base-300 bg-base-200 z-10 shadow-lg rounded-xl p-2 items-center'>
                 {/* tab selector */}
                 <div className='flex w-full h-10'>
-                    <div className='flex gap-2 items-center'>
-                        <button onClick={()=>{setTab(true)}} type='button' className={`h-8 transition-all duration-700 ${tab? "bg-neutral text-neutral-content border-none shadow-md/30 shadow-neutral": "bg-base-200 text-base-content border-[1px] border-base-content"}  rounded-lg w-20 items-center justify-center shrink-0 flex`}>Main</button>
-                        <button onClick={()=>{setTab(false)}} type='button' className={`h-8 transition-all duration-700 ${!tab? "bg-neutral text-neutral-content border-none shadow-md/30 shadow-neutral": "bg-base-200 text-base-content border-[1px] border-base-content"} rounded-lg w-20 items-center justify-center shrink-0 flex`}>Extra</button>
+                    <div className='flex items-center'>
+                        <button onClick={()=>{setTab(true)}} type='button' className={`h-full transition-all duration-700 ${tab? "bg-base-100 ": "bg-base-200"}  rounded-t-lg w-20 items-center justify-center shrink-0 flex`}>Main</button>
+                        <button onClick={()=>{setTab(false)}} type='button' className={`h-full transition-all duration-700 ${!tab? "bg-base-100 ": "bg-base-200"} rounded-t-lg w-20 items-center justify-center shrink-0 flex`}>Extra</button>
                     </div>
                     {/* title */}
-                    <div className='flex w-full items-center justify-center text-xl'>Create New Entry</div>
+                    <div className='flex w-full items-center h-8 justify-center text-xl'>Create New Entry</div>
                     {/* close button */}
-                    <button type='button' onClick={()=>{setShowForm(false)}} className='flex justify-center items-center rounded-lg bg-accent text-accent-content basis-12 hover:rounded-xl duration-500 transition-all'>&#x2715;</button>
+                    <button type='button' onClick={()=>{setShowForm(false)}} className='flex justify-center items-center rounded-lg bg-neutral font-bold text-xl text-accent-content h-8 w-14 hover:rounded-xl duration-500 transition-all'>&#x2715;</button>
                 </div>
 
                 {tab ? 
-                <div className='flex flex-col w-full h-full gap-5 p-2'>
+                <div className='flex flex-col w-full h-full gap-5 p-2 bg-base-100'>
                    
                    <div className='text-xl'>Set Main fields</div>
-                   <div className='flex w-full border-2 rounded-lg h-8 pl-2 focus-within:border-primary focus-within:shadow-lg/100 focus-within:shadow-zinc-300 transition-all duration-700'>
+                   <div className='flex w-full border-2 rounded-lg h-14 pl-2 focus-within:border-primary focus-within:shadow-lg/100 focus-within:shadow-zinc-300 transition-all duration-700'>
                             <div className='flex text-nowrap w-18 shrink md:w-20 lg:w-24 items-center text-lg'>Title</div>
-                            <input value={entry.title} id='title' className='flex w-full px-2 outline-none focus:bg-base-300 z-0 rounded-r-md bg-base-100' onChange={handleChange} />
+                            <input value={entry.title} placeholder='enter a title here' id='title' className='flex w-full px-2 outline-none focus:bg-base-300 z-0 rounded-r-md bg-base-100' onChange={handleChange} />
                         </div>
-                   <div className='flex w-full border-2 rounded-lg pl-2 focus-within:border-primary focus-within:shadow-lg/100 focus-within:shadow-zinc-300 transition-all duration-700'>
+                   <div className='flex w-full border-2 h-14 rounded-lg pl-2 focus-within:border-primary focus-within:shadow-lg/100 focus-within:shadow-zinc-300 transition-all duration-700'>
                         <div className='flex text-nowrap w-18 shrink md:w-20 lg:w-24 items-center text-md'>Username</div>
-                        <input value={entry.username} id='username' className='flex w-full px-2 outline-none focus:bg-base-300 z-0 rounded-r-md bg-base-300' onChange={handleChange} />
+                        <input value={entry.username} placeholder='enter a username here' id='username' className='flex w-full px-2 outline-none focus:bg-base-300 z-0 rounded-r-md bg-base-200' onChange={handleChange} />
                     </div>
 
-                    <div className='flex gap-2 items-center w-full h-8 border-2 rounded-lg pl-2 focus-within:border-primary focus-within:shadow-lg/100 focus-within:shadow-zinc-300 transition-all duration-700'>
-                        <div className='flex w-24 text-md items-center'>Password</div>
-                        <div className=" flex w-full rounded-r-md  items-center gap-1 px-1 focus-within:bg-base-300">
-                            <input type={showPass?'text':'password'} value={entry.password.toString() } id='password' className='flex w-full outline-none' onChange={handleChange} />
-                            <Image src={'/images/randomise.svg'} alt='randomise' width={25} height={25} className='h-auto flex' />
+                    <div className='flex gap-2 items-center w-full h-14 border-2 rounded-lg pl-2 focus-within:border-primary focus-within:shadow-lg/100 focus-within:shadow-zinc-300 transition-all duration-700'>
+                        <div className='flex w-18 text-md items-center'>Password</div>
+                        <div className=" flex w-full rounded-r-md h-full items-center gap-1 px-1 focus-within:bg-base-300 bg-base-200">
+                            <input type={showPass?'text':'password'} value={entry.password.toString() } placeholder='enter a password here or click randomise icon to create one' id='password' className='flex w-full outline-none' onChange={handleChange} />
+                            <Image onClick={()=>{setShowRandomPassModal(true)}} src={'/images/randomise.svg'} alt='randomise' width={25} height={25} className='h-auto flex' />
                             <Image onClick={()=>{setShowPass(!showPass)}} src={showPass? "/images/hidePass.svg": "/images/showPass.svg"} alt={showPass?'hide':'show'} width={25} height={25} className='w-auto h-auto cursor-pointer' title={showPass?'hide password':'show password'} />
                         </div>
                     </div>
-                    {showRandomPassModal && <RandomPassModal setShowRandomPassModal={setShowRandomPassModal}  setEntry={setEntry}/>}
                     <div className='flex flex-col gap-2 items-start w-full h-full'>
-                        <div className='flex text-nowrap w-34'>Notes:</div>
-                        <textarea value={entry.notes} id='notes' className='flex w-full h-2/3 resize-none px-1 border-2 focus:bg-base-300 outline-none rounded-lg focus:border-primary transition-all duration-700 focus:shadow-lg/100 shadow-zinc-300' onChange={handleChange} />
+                        <div className='flex text-nowrap w-full'>Notes:</div>
+                        <textarea value={entry.notes} id='notes' className='flex w-full h-full resize-none px-1 border-2 focus:bg-base-300 outline-none rounded-lg focus:border-primary transition-all duration-700 focus:shadow-lg/100 shadow-zinc-300' onChange={handleChange} />
                     </div>
                 </div>
                 :
-                <div className='flex flex-col w-full h-full gap-5 p-2'>
+                <div className='flex flex-col w-full h-full gap-5 p-2 bg-base-100'>
                     <div className='flex flex-col w-full h-fit'>
                         <div className='flex w-full h-fit justify-center text-lg'>New Extra field</div>
                         <div className='flex flex-col gap-5'>
