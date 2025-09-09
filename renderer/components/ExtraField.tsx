07f1c600baf93b493f0e6fc1804afd10e62dd8b6
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-09 15:13:08 +1000

    changed div elements to be input elements

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -9,16 +9,12 @@ type props = {
 export default function ExtraFieldComponent({extraField}:props) {
   return (
     <div className='flex flex-row w-full text-md gap-2 border-2 rounded-lg border-base-300 px-2 h-10 items-center'>
-        <div className='flex w-full'>
-        {extraField.name}
-        </div>
-        <div className='flex w-full peer-hover:'>
-          {extraField.data.toString()}
-        </div>
-        <div className='flex w-32'>
-        <input type='checkbox' checked={extraField.isSensitive}  />
-        </div>
+        <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' value={extraField.name} />
         
+        <input className='flex w-full border-r-2 border-base-300 h-full items-center outline-none' value={extraField.data.toString()} />
+        <div className='flex w-32 justify-end'>
+        <input type='checkbox' checked={extraField.isSensitive} className='flex'/>
+        </div>
     </div>
   )
 }
