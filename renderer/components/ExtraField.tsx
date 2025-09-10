591cb04728847a7e505e501ca8d086b90d57c203
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-10 11:15:59 +1000

    added readOnly property for inputs as to not allow users to change extra fields

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -9,11 +9,11 @@ type props = {
 export default function ExtraFieldComponent({extraField}:props) {
   return (
     <div className='flex flex-row w-full text-md gap-2 border-2 rounded-lg border-base-300 px-2 h-10 items-center'>
-        <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' value={extraField.name} />
+        <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' readOnly value={extraField.name} />
         
-        <input className='flex w-full border-r-2 border-base-300 h-full items-center outline-none' value={extraField.data.toString()} />
+        <input className='flex w-full border-r-2 border-base-300 h-full items-center outline-none' readOnly value={extraField.data.toString()} />
         <div className='flex w-32 justify-end'>
-        <input type='checkbox' checked={extraField.isSensitive} className='flex'/>
+        <input type='checkbox' checked={extraField.isSensitive} className='flex' readOnly/>
         </div>
     </div>
   )
