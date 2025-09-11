20cab4ed08709f057e3ee11bd729bb8e1ec9ebf8
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-12 06:54:23 +1000

    added a general purpose encrypt and decrypt to reduce repetition of same code

--- renderer/utils/commons.ts
+++ renderer/utils/commons.ts
@@ -15,6 +15,35 @@ export function isStrongPassword(password:string):string{
     return passwordMessage.length > 0? "The password "+passwordMessage.join(", ") : "";
 }
 
+export async function decrypt(content:Buffer| string, key: CryptoKey, iv:Buffer):Promise<{data:Buffer, status:"OK"| "ERROR"}>{
+    if (typeof window !== "undefined"){
+        const c = content instanceof Buffer? content : Buffer.from(content);
+        try {
+            return {
+                data: Buffer.from(await window.crypto.subtle.decrypt({ name:"AES-GCM", iv:Buffer.from(iv)}, key, Buffer.from(c))),   
+                status: "OK"
+            }
+        } catch (error) {
+            return{
+                data:Buffer.from(error),
+                status:"ERROR"
+            }
+        }
+    }
+    throw new Error("Window was not defined")
+
+}
+
+export async function encrypt(content:Buffer | string, key:CryptoKey){
+    if (typeof window !== 'undefined'){
+        const iv = window.crypto.getRandomValues(new Uint8Array(12));
+        const data = content instanceof Buffer? content : Buffer.from(content);
+        const encryptedText = await window.crypto.subtle.encrypt({name:"AES-GCM", iv}, key, Buffer.from(data));
+        return Buffer.concat([Buffer.from(encryptedText), iv])
+    }
+    throw new Error('Window object was undefined when trying to encrypt')
+}
+
 export const lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 export const upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 export const digits = ["0","1","2","3","4","5","6","7","8","9"];
