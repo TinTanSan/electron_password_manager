61d46b5fb2cafe2326b70720286fece0a78a15e8
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-12 11:12:53 +1000

    if decrypting the extrafield is not required then plainly set data as whatever is stored in extrafield

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ExtraField } from '../interfaces/Entry'
 import { VaultContext } from '../contexts/vaultContext'
 import Image from 'next/image';
+import { BannerContext } from '../contexts/bannerContext';
+import { addBanner } from '../interfaces/Banner';
 
 type props = {
     extraField: ExtraField,
@@ -11,17 +13,34 @@ type props = {
 
 export default function ExtraFieldComponent({extraField, uuid}:props) {
   const {vault} = useContext(VaultContext);
-  const [data, setData] = useState();
-
+  const bannerContext = useContext(BannerContext);
+  const [data, setData] = useState<undefined | string>(undefined);
+  const [showData, setShowData] = useState(false);
   useEffect(()=>{
-    vault.entries.find(x=>x.metadata.uuid === uuid)
+    // ensure only decrypting when extrafield is sensitive, and ensure vault kek is present
+    if (extraField.isSensitive && vault.kek){
+      vault.entries.find(x=>x.metadata.uuid === uuid).decryptExtraField(extraField.name,vault.kek).then((decryptedD)=>{
+        if (decryptedD.status === 'ERROR'){
+          addBanner(bannerContext, 'error decrypting extra field data', 'error');
+          console.error(decryptedD.data.toString());
+        }else{
+          setData(decryptedD.data.toString());
+        }
+      })
+    }else{
+      setData(extraField.data.toString());
+    }
   },[extraField])
 
   return (
     <div className='flex flex-row w-full text-md gap-2 border-2 rounded-lg border-base-300 px-2 h-10 items-center'>
         <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' readOnly value={extraField.name} />
+        <div className='flex w-full h-full border-2 broder-base-300 rounded-lg items-center px-2'>
+          <input className='flex w-full border-r-2 border-base-300 h-full items-center outline-none' readOnly value={data?data: "*".repeat(Math.max(4,Math.floor(Math.random()*10)))} />
+          {extraField.isSensitive && <Image src={showData?'/images/eye-off.svg':'/images/eye.svg'} alt='show/hide' width={25} height={25} className='flex w-auto bg-base-200 hover:cursor-pointer' onClick={()=>{setShowData(prev=>!prev)}} />}
+        </div>
         
-        <input className='flex w-full border-r-2 border-base-300 h-full items-center outline-none' readOnly value={extraField.data.toString()} />
+        
         <div className='flex w-32 justify-end'>
           <input type='checkbox' checked={extraField.isSensitive} className='flex' readOnly/>
         </div>
