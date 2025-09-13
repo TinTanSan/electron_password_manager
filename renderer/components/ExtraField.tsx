8ca28bc55cb2857c2c560a6e003b27ee50b07e91
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-13 17:58:32 +1000

    now taking entry as a prop and changed decryption to occur when showData is true.

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ExtraField } from '../interfaces/Entry'
+import { Entry, ExtraField } from '../interfaces/Entry'
 import { VaultContext } from '../contexts/vaultContext'
 import Image from 'next/image';
 import { BannerContext } from '../contexts/bannerContext';
@@ -7,37 +7,44 @@ import { addBanner } from '../interfaces/Banner';
 
 type props = {
     extraField: ExtraField,
-    uuid: string,
+    entry:Entry,
     onDelete: (name:string)=>void 
 }
 
-export default function ExtraFieldComponent({extraField, uuid, onDelete}:props) {
+export default function ExtraFieldComponent({extraField, entry, onDelete}:props) {
   const {vault} = useContext(VaultContext);
   const bannerContext = useContext(BannerContext);
   const [data, setData] = useState<undefined | string>(undefined);
   const [showData, setShowData] = useState(false);
   useEffect(()=>{
-    // ensure only decrypting when extrafield is sensitive, and ensure vault kek is present
-    if (extraField.isSensitive && vault.kek){
-      vault.entries.find(x=>x.metadata.uuid === uuid).decryptExtraField(extraField.name,vault.kek).then((decryptedD)=>{
-        if (decryptedD.status === 'ERROR'){
-          addBanner(bannerContext, 'error decrypting extra field data', 'error');
-          console.error(decryptedD.data.toString());
-        }else{
-          setData(decryptedD.data.toString());
-        }
-      })
-    }else{
-      setData(extraField.data.toString());
-    }
+    setShowData(false)
   },[extraField])
+
+  useEffect(()=>{
+    if (extraField.isSensitive && showData && !data){
+        entry.decryptExtraField(extraField.name, vault.kek).then((d)=>{
+          console.log(d.data.toString())
+          if (d.status === "ERROR"){
+            addBanner(bannerContext, 'error decrypting extra field data', 'error')
+            setData(undefined);
+          }else{
+            setData(d.data.toString());
+          }
+        })
+    }else if (!showData){
+      setData(undefined);
+    }
+  },[showData])
+
+
+
 
   return (
     <div className='flex flex-row w-full text-md items-center gap-2'>
       <div className='flex flex-row w-full gap-2 border-2 rounded-lg border-base-300 px-2 h-10 items-center'>
-        <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' value={extraField.name} />
+        <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' defaultValue={extraField.name} onChange={(e)=>{e.target.value = extraField.name}}/>
         <div className='flex w-full h-full border-r-2 border-base-300 items-center px-2'>
-          <input className='flex w-full border-base-300 h-full items-center outline-none' defaultValue={(!extraField.isSensitive || showData)?data: "*".repeat(Math.max(8,Math.floor(Math.random()*8)))} readOnly/>
+          <input className='flex w-full border-base-300 h-full items-center outline-none' value={(!extraField.isSensitive || showData)? data : "*".repeat(6)} onChange={(e)=>{e.target.value = data? data: "*".repeat(6)}}/>
           {extraField.isSensitive && <Image src={showData?'/images/hidePass.svg':'/images/showPass.svg'} alt='show/hide' width={25} height={25} className='flex w-auto hover:cursor-pointer' onClick={()=>{setShowData(prev=>!prev)}} />}
         </div>
         
@@ -46,7 +53,7 @@ export default function ExtraFieldComponent({extraField, uuid, onDelete}:props) 
           <input type='checkbox' checked={extraField.isSensitive} className='flex' readOnly/>
         </div>
         </div>
-        <Image src={'/images/delete.svg'} alt='del' width={10} height={10} onClick={()=>{onDelete(extraField.name)}} className='flex w-auto h-8 hover:bg-error rounded-md  cursor-pointer'/>
+        <Image src={'/images/delete.svg'} alt='del' width={10} height={10} onClick={()=>{onDelete(extraField.name)}} className='flex w-auto h-6 hover:bg-error rounded-md  cursor-pointer'/>
         
     </div>
   )
