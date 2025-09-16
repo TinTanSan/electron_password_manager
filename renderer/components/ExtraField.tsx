c77ef33aa5721e9e43b1d4818f66900d6ec958eb
TinTanSan <tirthvpatel2003@gmail.com>
2025-09-16 14:58:15 +1000

    updated extraField component to use the isProtected attribtue, and also updated the conditionally injected value of the data input box where before there was a bug in which if the extra field wasn't protected then it wouldn't show the data

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -21,7 +21,7 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
   },[extraField])
 
   useEffect(()=>{
-    if (extraField.isSensitive && showData && !data){
+    if (extraField.isProtected && showData && !data){
         entry.decryptExtraField(extraField.name, vault.kek).then((d)=>{
           console.log(d.data.toString())
           if (d.status === "ERROR"){
@@ -44,17 +44,16 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
       <div className='flex flex-row w-full gap-2 border-2 rounded-lg border-base-300 px-2 h-10 items-center'>
         <input className='flex w-full h-full items-center border-r-2 border-base-300 outline-none' defaultValue={extraField.name} onChange={(e)=>{e.target.value = extraField.name}}/>
         <div className='flex w-full h-full border-r-2 border-base-300 items-center px-2'>
-          <input className='flex w-full border-base-300 h-full items-center outline-none' value={(!extraField.isSensitive || showData)? data : "*".repeat(6)} onChange={(e)=>{e.target.value = data? data: "*".repeat(6)}}/>
-          {extraField.isSensitive && <Image src={showData?'/images/hidePass.svg':'/images/showPass.svg'} alt='show/hide' width={25} height={25} className='flex w-auto hover:cursor-pointer' onClick={()=>{setShowData(prev=>!prev)}} />}
+          <input className='flex w-full border-base-300 h-full items-center outline-none' value={(extraField.isProtected)? (showData ? data : "*".repeat(6)) : extraField.data.toString()} onChange={(e)=>{e.target.value = data? data: "*".repeat(6)}}/>
+          {extraField.isProtected && <Image src={showData?'/images/hidePass.svg':'/images/showPass.svg'} alt='show/hide' width={25} height={25} className='flex w-auto hover:cursor-pointer' onClick={()=>{setShowData(prev=>!prev)}} />}
         </div>
         
         
         <div className='flex w-32 justify-end'>
-          <input type='checkbox' checked={extraField.isSensitive} className='flex' readOnly/>
+          <input type='checkbox' checked={extraField.isProtected} className='flex' readOnly/>
         </div>
         </div>
         <Image src={'/images/delete.svg'} alt='del' width={10} height={10} onClick={()=>{onDelete(extraField.name)}} className='flex w-auto h-6 hover:bg-error rounded-md  cursor-pointer'/>
-        
     </div>
   )
 }
