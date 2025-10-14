f6fbad0c9641b3f4ade8d5a0b2c741166b1ee7b8
TinTanSan <tirthvpatel2003@gmail.com>
2025-10-14 12:45:13 +1100

    removed functions for handleClose and handleLock as they have been moved into the sidebar component

--- renderer/components/Navbar.tsx
+++ renderer/components/Navbar.tsx
@@ -20,33 +20,11 @@ export default function Navbar({search, setSearch, searchSettings, setSearchSett
     const [showEntryForm, setShowEntryForm] = useState(false)
 
 
-    const handleClose = ()=>{
-        bannerContext.setBanners([])
-        addBanner(bannerContext, "Vault Closed successfully", 'info')
-        setVault(undefined);
-    }
-
-    const handleLock = ()=>{
-        window.ipc.clearClipboard();
-        window.ipc.openFile(vault.filePath).then((content)=>{
-            setVault(prev=>(new Vault({...prev, fileContents:content.fileContents, isUnlocked:false})))
-            bannerContext.setBanners([]);
-            addBanner(bannerContext, 'Vault locked', 'info');
-        })
-    }
-
-
     return (
-        <div className='flex flex-row w-full h-12 items-center justify-between px-1 shadow-lg bg-base-100 border-2 border-base-300 rounded-xl'>
+        <div className='flex flex-row w-full h-12 items-center justify-between p-1 gap-5'>
             {showEntryForm && <NewEntryForm setShowForm={setShowEntryForm}/>}
-            <button onClick={()=>{setShowEntryForm(true)}} className='flex bg-primary hover:bg-primary-darken text-primary-content w-32 shrink-0 h-8 justify-center items-center rounded-lg text-nowrap'>New Entry</button>
-            <div className='flex justify-center w-full px-5 text-xl shrink grow'>
-                <SearchBar value={search} setValue={setSearch} settings={searchSettings} setSearchSettings={setSearchSettings}/>
-            </div>
-            <div className='flex w-fit gap-2 ' >
-                <button onClick={handleLock} className='flex bg-accent hover:bg-accent-darken shrink-0 text-accent-content w-24 justify-center items-center rounded-lg h-8' title='locking will send you back to the unlock page'>Lock</button>
-                <button onClick={handleClose} className='flex bg-primary hover:bg-primary-darken shrink-0 text-primary-content w-24 justify-center items-center rounded-lg h-8'  title='closing will allow you to open anohter vault'>Close Vault</button>
-            </div>
+            <SearchBar value={search} setValue={setSearch} settings={searchSettings} setSearchSettings={setSearchSettings}/>
+            <button onClick={()=>{setShowEntryForm(true)}} className='flex bg-neutral hover:bg-neutral-darken text-neutral-content sm:w-30 md:w-48 shrink h-full justify-center items-center rounded-md text-nowrap'>+ New Entry</button>
         </div>
   )
 }
