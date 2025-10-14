1ce9b785e5ce26ee46903a2c07e8cefc16e0d7ef
TinTanSan <tirthvpatel2003@gmail.com>
2025-10-14 12:44:09 +1100

    minor changes to styling to align with new design

--- renderer/components/searchBar.tsx
+++ renderer/components/searchBar.tsx
@@ -16,11 +16,11 @@ type props ={
 
 export default function SearchBar({value, setValue, settings, setSearchSettings}:props) {
   return (
-    <div className='flex w-full shrink grow border-2 rounded-xl items-center px-1 h-9 gap-1'>
-        <input value={value} onChange={(e)=>{setValue(e.target.value)}} className='w-full outline-none' placeholder={`search for an entry`} />
-        <button title='allow searching through titles' onClick={()=>{setSearchSettings((prev)=>({...prev, searchTitle:!prev.searchTitle}))}} className={`w-7 h-6 text-sm border-2 flex items-center justify-center rounded-lg ${settings.searchTitle&& 'bg-neutral text-white border-none font-bold'}`}>T</button>
-        <button title='allow searching through usernames' onClick={()=>{setSearchSettings((prev)=>({...prev, searchUsername:!prev.searchUsername}))}} className={`w-7 h-6 text-sm border-2 flex items-center justify-center rounded-lg ${settings.searchUsername&& 'bg-neutral text-white border-none font-bold'}`}>U</button>
-        <button title='allow searching through notes' onClick={()=>{setSearchSettings((prev)=>({...prev, searchNotes:!prev.searchNotes}))}} className={`w-7 h-6 text-sm border-2 flex items-center justify-center rounded-lg ${settings.searchNotes&& 'bg-neutral text-white border-none font-bold'}`}>N</button>
+    <div className='flex w-full shrink grow border-2 rounded-md items-center px-1 h-full gap-1'>
+        <input value={value} onChange={(e)=>{setValue(e.target.value)}} className='w-full outline-none placeholder:font-bold placeholder:text-neutral placeholder:opacity-50' placeholder='search' />
+        <button title='allow searching through titles' onClick={()=>{setSearchSettings((prev)=>({...prev, searchTitle:!prev.searchTitle}))}} className={`w-6 h-6 text-sm border-2 flex items-center justify-center rounded-lg ${settings.searchTitle&& 'bg-neutral text-white border-none font-bold'}`}>T</button>
+        <button title='allow searching through usernames' onClick={()=>{setSearchSettings((prev)=>({...prev, searchUsername:!prev.searchUsername}))}} className={`w-6 h-6 text-sm border-2 flex items-center justify-center rounded-lg ${settings.searchUsername&& 'bg-neutral text-white border-none font-bold'}`}>U</button>
+        <button title='allow searching through notes' onClick={()=>{setSearchSettings((prev)=>({...prev, searchNotes:!prev.searchNotes}))}} className={`w-6 h-6 text-sm border-2 flex items-center justify-center rounded-lg ${settings.searchNotes&& 'bg-neutral text-white border-none font-bold'}`}>N</button>
     </div>
   )
 }
