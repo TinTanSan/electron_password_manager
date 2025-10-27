7cef5dcc41c3ba8a93832f9cae57c8cda86d0c17
TinTanSan <tirthvpatel2003@gmail.com>
2025-10-28 09:01:39 +1100

    implementing new ui

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -16,6 +16,23 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
   const bannerContext = useContext(BannerContext);
   const [data, setData] = useState<undefined | string>(undefined);
   const [showData, setShowData] = useState(false);
+  const [ef, setEf] = useState<ExtraField>({...extraField});
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
+    setEf(prev=>({
+        ...prev,
+        [e.target.id] : e.target.id === 'data'? Buffer.from(e.target.value): e.target.value
+      })
+    )
+  }
+
+  const handleConfirm = (e:React.MouseEvent)=>{
+    e.preventDefault();
+    entry.addExtraField(vault.kek, {...ef}).then((newState)=>{
+      vault.mutate('entries',[vault.entries.filter(x=>x.metadata.uuid !== entry.metadata.uuid),newState])
+    }) 
+  }
+
   useEffect(()=>{
     setShowData(false)
   },[extraField])
@@ -45,20 +62,24 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
 
 
   return (
-    <div className='flex flex-row w-full h-40 shrink-0 border-2 border-base-300 bg-base-100 rounded-lg p-2 gap-2'>
-      <div className='flex flex-col w-full h-full gap-2'>
-        <div className='flex flex-row border-2 border-base-300 rounded-lg focus-within:border-primary'>
-          <div className='flex border-r-2 border-base-300 px-1'>Name</div>
-          <input className='flex w-full h-full bg-base-100 px-1 rounded-r-lg outline-none' readOnly value={extraField.name}/>
+    <div className='flex flex-col w-full h-80 shrink-0 border-2 rounded-lg border-base-300 p-2 gap-2 bg-base-100'>
+      <div className='flex flex-col w-full h-full'>
+        <div className='flex flex-col w-full gap-2'>
+          <div>Name</div>
+          <input className='flex border-2 rounded-md items-center px-1 h-8 border-base-300' readOnly value={extraField.name}/>
         </div>
-        <div className='flex flex-row w-full h-fit gap-2'>
-          <button onClick={()=>{handleDelete()}} className='flex w-1/2 items-center justify-center rounded-lg h-8 bg-error hover:bg-error-darken hover:text-white text-white'>Delete</button>
-          {extraField.isProtected && <button onClick={()=>{setShowData(prev=>!prev)}} className={`flex border-2 w-1/2 items-center justify-center rounded-lg h-8 border-neutral ${showData&& "bg-neutral text-neutral-content"}`}>{showData?"Hide" : "Reveal"}</button>}
+        <div className='flex flex-col w-full h-full gap-2'>
+          <div>Data</div>
+          <textarea className='flex border-2 rounded-lg h-full resize-none px-1' readOnly value={data ? data: "Click Reveal to show "} />
+          <button className='flex border-2 border-neutral rounded-lg h-8 items-center justify-center w-1/2' onClick={()=>{setShowData(!showData)}}>Reveal</button>
         </div>
       </div>
-      <div className='flex flex-col w-full h-full'>
-        <textarea value={(!extraField.isProtected  || showData)?data:"Click reveal to show contents"}  readOnly className='flex p-1 overflow-y-auto border-2 border-base-300 resize-none h-full w-full rounded-lg bg-base-100 text-base-content'/>
+      <div className='flex flex-row w-full h-10 gap-2'>
+        <button onClick={()=>{handleDelete()}} className='flex w-1/2 items-center justify-center rounded-lg h-8 bg-error hover:bg-error-darken hover:text-white text-white'>Delete</button>
+        <button className='flex justify-center items-center w-1/2 border-2 rounded-lg' onClick={handleConfirm}>Save Edits</button>
       </div>
     </div>
+
   )
 }
+''
