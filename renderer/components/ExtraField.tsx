b0f0cc8dbf07f9732cb215849abfaa4f113c4cf9
TinTanSan <tirthvpatel2003@gmail.com>
2025-10-28 15:47:14 +1100

    added handler which allows user to change whether the protect flag is active or not

--- renderer/components/ExtraField.tsx
+++ renderer/components/ExtraField.tsx
@@ -4,6 +4,7 @@ import { VaultContext } from '../contexts/vaultContext'
 import Image from 'next/image';
 import { BannerContext } from '../contexts/bannerContext';
 import { addBanner } from '../interfaces/Banner';
+import { encrypt } from '../utils/commons';
 
 type props = {
     extraField: ExtraField,
@@ -17,7 +18,9 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
   const [data, setData] = useState<undefined | string>(undefined);
   const [showData, setShowData] = useState(false);
   const [ef, setEf] = useState<ExtraField>({...extraField});
-
+  const [encryptedData, setEncryptedData] = useState<undefined | Buffer>(ef.isProtected?ef.data : undefined);
+  const hasChanged = (ef.name === extraField.name && extraField.data.equals(ef.data) && ef.isProtected == extraField.isProtected);
+  console.log(ef.name === extraField.name , extraField.data.equals(ef.data) , ef.isProtected == extraField.isProtected);
   const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
     setEf(prev=>({
         ...prev,
@@ -31,6 +34,33 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
     entry.addExtraField(vault.kek, {...ef}).then((newState)=>{
       vault.mutate('entries',[vault.entries.filter(x=>x.metadata.uuid !== entry.metadata.uuid),newState])
     }) 
+  }
+  // we assume that just clicking on the Expose/Protect button doesn't automatically update the field in the vault, the user must
+  // confirm the action with the save button at the bottom.
+  const handleChangeProtection = ()=>{
+    if (ef.isProtected){
+      if (encryptedData === undefined){
+        setEncryptedData(ef.data);
+      }
+      entry.decryptExtraField(ef.name, vault.kek).then((results)=>{
+        if (results.status === "OK"){
+          setEf(prev=>({...prev, data:results.data, isProtected:false}));
+          setData(results.data.toString());
+        }else{
+          addBanner(bannerContext, "Unable to permanently expose extrafield data because something went wrong decrypting the data", "error");
+        }
+      })
+    }else{
+      if (encryptedData === undefined){
+        entry.encryptField(vault.kek, ef.name, ef.data).then((encryptedEf)=>{
+          setEf(encryptedEf)
+        });
+      }else{
+        setEf(prev=>({...prev, isProtected:true, data: encryptedData}));
+      }
+      setShowData(false);
+      setData(undefined);
+    }
   }
 
   useEffect(()=>{
@@ -66,17 +96,20 @@ export default function ExtraFieldComponent({extraField, entry, onDelete}:props)
       <div className='flex flex-col w-full h-full'>
         <div className='flex flex-col w-full gap-2'>
           <div>Name</div>
-          <input className='flex border-2 rounded-md items-center px-1 h-8 border-base-300' readOnly value={extraField.name}/>
+          <input className='flex border-2 rounded-md items-center px-1 h-8 border-base-300' readOnly value={ef.name}/>
         </div>
         <div className='flex flex-col w-full h-full gap-2'>
           <div>Data</div>
           <textarea className='flex border-2 rounded-lg h-full resize-none px-1' readOnly value={data ? data: "Click Reveal to show "} />
-          <button className='flex border-2 border-neutral rounded-lg h-8 items-center justify-center w-1/2' onClick={()=>{setShowData(!showData)}}>Reveal</button>
+          <div className='flex w-full h-8 gap-2'>
+          {ef.isProtected && <button className='flex border-2 border-neutral rounded-lg h-8 items-center justify-center w-1/2' onClick={()=>{setShowData(!showData)}}>Reveal</button>}
+          <button onClick={handleChangeProtection} className='flex border-2 border-warning text-warning-content w-1/2 items-center justify-center rounded-lg hover:bg-warning '>{ef.isProtected? "Expose":"Protect"}</button>
+          </div>
         </div>
       </div>
       <div className='flex flex-row w-full h-10 gap-2'>
-        <button onClick={()=>{handleDelete()}} className='flex w-1/2 items-center justify-center rounded-lg h-8 bg-error hover:bg-error-darken hover:text-white text-white'>Delete</button>
-        <button className='flex justify-center items-center w-1/2 border-2 rounded-lg' onClick={handleConfirm}>Save Edits</button>
+        <button onClick={()=>{handleDelete()}} className='flex w-1/2 items-center cursor-pointer justify-center rounded-lg h-8 bg-error hover:bg-error-darken hover:text-white text-white'>Delete</button>
+        {!hasChanged && <button className='flex justify-center items-center w-1/2 border-2 rounded-lg cursor-pointer' onClick={handleConfirm}>Save Edits</button>}
       </div>
     </div>
 
