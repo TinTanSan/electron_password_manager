f3138b7fe7785ae1e11949a230f7de23e884bf95
TinTanSan <tirthvpatel2003@gmail.com>
2025-11-03 15:11:24 +1100

    reduced gap between the entry icon and the title

--- renderer/components/EntryComponent.tsx
+++ renderer/components/EntryComponent.tsx
@@ -81,7 +81,7 @@ export default function EntryComponent({entry, groups}:props) {
     return (
         <div className={`flex flex-col w-full transition-all duration-500 overflow-hidden ${extend?'h-56':"h-14 items-center "} border-2  justify-between px-2 gap-2 rounded-lg border-base-300 bg-base-100 `}>
             {showEditModal && <EntryModal groups={groups} setShowModal={setShowEditModal} uuid={entry.metadata.uuid}/>}
-            <div onClick={()=>{setExtend(prev=>!prev)}} className='flex w-full h-12 grow-0 shrink-0 items-center px2 gap-5'>
+            <div onClick={()=>{setExtend(prev=>!prev)}} className='flex w-full h-12 grow-0 shrink-0 items-center px2 gap-2'>
                 <Image src={"/images/defaultGroup.svg"} alt="entry" width={0} height={0} className='flex w-8 h-auto shrink-0 grow-0' />
                 <div className='flex h-12 w-full grow shrink text-nowrap overflow-hidden overflow-ellipsis items-center text-lg font-[500]'>{entry.title? entry.title : <i>No title</i>}</div>
                 <div className='flex w-fit justify-end'>
