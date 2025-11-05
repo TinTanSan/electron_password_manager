34bade1faecfc26c31f83224f66f9817cdf5ff8c
TinTanSan <tirthvpatel2003@gmail.com>
2025-11-05 15:23:46 +1100

    added a containing div to add a favourite star to the top left of each component that has been favourited

--- renderer/components/EntryComponent.tsx
+++ renderer/components/EntryComponent.tsx
@@ -78,39 +78,41 @@ export default function EntryComponent({entry}:props) {
     }
     
     return (
-        <div className={`flex flex-col w-full transition-all duration-500 ${extend?'h-56':"h-14 items-center"} overflow-hidden border-2 relative px-2 gap-2 rounded-lg border-base-300 bg-base-100 `}>
-            {entry.isFavourite && <Image  src={"/images/starFill.svg"} alt='fav' width={20} height={20} className='flex absolute -left-1.5 z-10'/>    }
-            {showEditModal && <EntryModal setShowModal={setShowEditModal} uuid={entry.metadata.uuid}/>}
-            <div onClick={()=>{setExtend(prev=>!prev)}} className='flex w-full h-12 grow-0 shrink-0 items-center px2 gap-2'>
-                <Image src={"/images/defaultGroup.svg"} alt="entry" width={0} height={0} className='flex w-8 h-auto shrink-0 grow-0' />
-                <div className='flex h-12 w-full grow shrink text-nowrap overflow-hidden overflow-ellipsis items-center text-lg font-[500]'>
-                    {entry.title? entry.title : <i>No title</i>}
+        <div className='flex w-full h-fit relative'>
+            {entry.isFavourite && <Image src={'/images/starFill.svg' } alt='fav' width={25} height={25} className='flex absolute -left-2 -top-2 z-10' />}
+            <div className={`flex flex-col w-full transition-all duration-500 ${extend?'h-56':"h-14 items-center"} overflow-hidden border-2 relative px-2 gap-2 rounded-lg border-base-300 bg-base-100 `}>
+                {showEditModal && <EntryModal setShowModal={setShowEditModal} uuid={entry.metadata.uuid}/>}
+                <div onClick={()=>{setExtend(prev=>!prev)}} className='flex w-full h-12 grow-0 shrink-0 items-center px2 gap-2'>
+                    <Image src={"/images/defaultGroup.svg"} alt="entry" width={0} height={0} className='flex w-8 h-auto shrink-0 grow-0' />
+                    <div className='flex h-full w-full px-2 justify-start text-nowrap overflow-hidden overflow-ellipsis items-center text-lg font-[500]'>
+                        {entry.title? entry.title : <i>No title</i>}
+                    </div>
+                    <div className='flex w-fit justify-end'>
+                        <Image src={`/images/${extend?'collapse':'expand'}.svg`} alt='expand' width={0} height={0} className='flex w-6 h-auto  shrink-0 grow-0'/>
+                    </div>
                 </div>
-                <div className='flex w-fit justify-end'>
-                    <Image src={`/images/${extend?'collapse':'expand'}.svg`} alt='expand' width={0} height={0} className='flex w-6 h-auto  shrink-0 grow-0'/>
-                </div>
-            </div>
-            <div className={`flex text-md font-normal flex-col w-full text-base-content p-2 border-2 overflow-hidden ${extend ? 'h-full visible': 'collapse'}`}>
-                <div className='flex flex-col w-fit h-full gap-2 overflow-hidden overflow-ellipsis'>
-                    <div className='flex w-full h-6 gap-2'>
-                        <div className='flex w-32 h-full'>Username</div>
-                        <div className='flex w-full h-full'>{entry.username}</div>
+                <div className={`flex text-md font-normal flex-col w-full text-base-content p-2 overflow-hidden ${extend ? 'h-full visible': 'collapse'}`}>
+                    <div className='flex flex-col w-fit h-full gap-2 overflow-hidden overflow-ellipsis'>
+                        <div className='flex w-full h-6 gap-2'>
+                            <div className='flex w-32 h-full'>Username</div>
+                            <div className='flex w-full h-full'>{entry.username}</div>
 
+                        </div>
+                        <div className='flex w-full h-6 gap-2'>
+                            <div className='flex w-32 h-full'>Password</div>
+                            <div className={` flex w-full h-full ${(showPass && !decryptedPass) && "italic"}`}>{showPass? decryptedPass?decryptedPass : "no pass to show" : "*".repeat(8)}</div>
+                            <Image onClick={handleCopy} src={"/images/copy.svg"} alt='copy' width={0} height={0} className='flex w-6 h-6 ' />
+                            <Image onClick={handleShowPass} src={showPass?"/images/hidePass.svg" : "/images/showPass.svg"} alt='show' width={0} height={0} className='flex w-6 h-6'/>
+                        </div>
+                        <div className='flex w-full h-6 gap-2'>
+                            <div className='flex w-32 h-full'>URL</div>
+                            <div className='flex w-full h-full'>This feature to come soon</div>
+                        </div>
                     </div>
-                    <div className='flex w-full h-6 gap-2'>
-                        <div className='flex w-32 h-full'>Password</div>
-                        <div className={` flex w-full h-full ${(showPass && !decryptedPass) && "italic"}`}>{showPass? decryptedPass?decryptedPass : "no pass to show" : "*".repeat(8)}</div>
-                        <Image onClick={handleCopy} src={"/images/copy.svg"} alt='copy' width={0} height={0} className='flex w-6 h-6 ' />
-                        <Image onClick={handleShowPass} src={showPass?"/images/hidePass.svg" : "/images/showPass.svg"} alt='show' width={0} height={0} className='flex w-6 h-6'/>
+                    <div className='flex flex-row w-full h-14 gap-2'>
+                        <button onClick={()=>{handleDelete()}} className='flex items-center justify-center text-xl rounded-lg border-error text-error hover:text-error-content hover:bg-error transition-all duration-300 border-2 w-full h-full'>Delete</button>
+                        <button onClick={()=>{setShowEditModal(true)}} className='flex items-center justify-center text-xl rounded-lg hover:text-info-content hover:bg-info outline-none duration-300 transition-all border-info border-2 w-full h-full'>Details &amp; Edit</button>
                     </div>
-                    <div className='flex w-full h-6 gap-2'>
-                        <div className='flex w-32 h-full'>URL</div>
-                        <div className='flex w-full h-full'>This feature to come soon</div>
-                    </div>
-                </div>
-                <div className='flex flex-row w-full h-14 gap-2'>
-                    <button onClick={()=>{handleDelete()}} className='flex items-center justify-center text-xl rounded-lg border-error text-error hover:text-error-content hover:bg-error transition-all duration-300 border-2 w-full h-full'>Delete</button>
-                    <button onClick={()=>{setShowEditModal(true)}} className='flex items-center justify-center text-xl rounded-lg hover:text-info-content hover:bg-info outline-none duration-300 transition-all border-info border-2 w-full h-full'>Details &amp; Edit</button>
                 </div>
             </div>
         </div>
