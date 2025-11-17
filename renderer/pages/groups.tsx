180d1465489f157993f6d59f2163f1ac17487425
TinTanSan <tirthvpatel2003@gmail.com>
2025-11-17 13:35:04 +1100

    added a simple list to show each group and number of entries in each group

--- renderer/pages/groups.tsx
+++ renderer/pages/groups.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Sidebar from '../components/Sidebar'
+import { VaultContext } from '../contexts/vaultContext'
+import { useRouter } from 'next/navigation';
 
 export default function GroupsPage() {
+    const {vault, setVault} = useContext(VaultContext);
   return (
-    <div className='flex w-screen h-screen overflow-hidden'>
+    <div className='flex w-screen h-screen overflow-hidden bg-gradient-to-b from-base-200 to-base-300'>
         <title>Groups</title>
         <Sidebar />
         {/* main content */}
-        <div className='flex w-full h-full'>
-
+        <div className='flex w-full h-full p-2'>
+            <div className='flex flex-col w-full h-full gap-2'>
+                {vault?.entryGroups.map((group, i)=>
+                    <div key={i} className='flex border-2 h-12 w-full rounded-lg border-base-300 bg-white px-2 items-center text-md font-[500] gap-5'>
+                        <div className='flex w-full'>
+                            {group.groupName}
+                        </div>
+                        <p className='flex w-40 justify-end text-nowrap font-normal text-md'>
+                            {group.entries.length} Entries
+                        </p>
+                    </div>
+                )}
+            </div>
         </div>
     </div>
   )
