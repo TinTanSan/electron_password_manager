cb5b1559da2a41819e6209b8b0b42447a9b298b3
TinTanSan <tirthvpatel2003@gmail.com>
2025-11-20 15:06:05 +1100

    added a context placeholder for preferences

---
+++ renderer/contexts/preferencesContext.tsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+export type PreferenceType = {
+    vaultLockTimeOut: number; //  seconds
+    keyRotationPeriod: number; // days
+    requireStrongMasterPassword: boolean;
+    entriesPerPage: number;
+    maxGeneratedPassLength: number; //characters
+    clearClipboardTime: number; //seconds
+}
+
+type PreferenceCTXType = {
+    preference: PreferenceType | undefined,
+    setPreference: React.Dispatch<React.SetStateAction<PreferenceType>>
+}
+
+
+export const PreferenceContext = createContext<PreferenceCTXType>({
+    preference: undefined,
+    setPreference:(_:PreferenceType)=>{throw new Error('Set prefence called outside of provider')} 
+})
