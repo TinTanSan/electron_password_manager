62cd14ee50634308b129fb2c1670ad71b855d98b
TinTanSan <tirthvpatel2003@gmail.com>
2025-11-25 18:38:28 +1100

    added extra preference settings and provided a defaults that can be used

--- renderer/contexts/preferencesContext.tsx
+++ renderer/contexts/preferencesContext.tsx
@@ -9,7 +9,21 @@ export type PreferenceType = {
     clearClipboardTime: number; //seconds
     fontSize: number;
     fontSpacing: number;
+    openLastOpenedVault: boolean; // like how VS code opens the files that you were working on previously rahter than finding them again
 }
+
+const preferenceDefaults: PreferenceType = {
+    vaultLockTimeOut: 30,
+    keyRotationPeriod: 90,
+    requireStrongMasterPassword: true,
+    entriesPerPage: 50,
+    maxGeneratedPassLength: 50,
+    clearClipboardTime: 10,
+    fontSize: 14,
+    fontSpacing: 1,
+    openLastOpenedVault: true
+}
+
 
 type PreferenceCTXType = {
     preference: PreferenceType | undefined,
