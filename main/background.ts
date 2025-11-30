1f26880e60d23e92dc202596067aafb715ed2095
TinTanSan <tirthvpatel2003@gmail.com>
2025-11-30 14:39:46 +1100

    placed file IPC handlers in their own file and imported on ready

--- main/background.ts
+++ main/background.ts
@@ -1,13 +1,10 @@
 import path from 'path'
-import { app, BrowserWindow, clipboard, dialog, globalShortcut, ipcMain, Menu } from 'electron'
+import { app, clipboard, ipcMain } from 'electron'
 import serve from 'electron-serve'
 import { createWindow} from './helpers'
-import fs from 'fs';
-import { MenuItem } from 'electron';
 import {setupMenus} from './helpers/setupMenus';
-const isProd = process.env.NODE_ENV === 'production'
-const data_path = app.getPath('userData');
 
+const isProd = process.env.NODE_ENV === 'production'
 
 if (isProd) {
   serve({ directory: 'app' })
@@ -44,6 +41,10 @@ export const createNextronWindow = async () => {
 
 createNextronWindow()
 
+app.whenReady().then(()=>{
+  import('./ipcHandlers/fileIPCHandlers');
+})
+
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
@@ -54,89 +55,6 @@ ipcMain.handle('message', async (_, arg) => {
   return "hello "+arg
 })
 
-ipcMain.handle('fileDialog:open', async()=>{
-  const fileDialog = await dialog.showOpenDialog({properties:['openFile']});
-  const fileOpened = ( fileDialog).filePaths[0];
-  if (!fileDialog.canceled){    
-    if (!fileOpened.endsWith(".vlt")){
-      return {fileContents:"INVALIDFILE", filePath:fileOpened, status:"INVALIDEXT"};
-    }
-    const fileContents =await fs.promises.readFile(fileOpened, 'utf-8');
-    handleAddRecent(fileOpened);
-    return {fileContents,filePath:fileOpened, status:"OK"}
-  }
-  return {fileContents:undefined, filePath:fileOpened, status:"CANCELLED"}
-  
-})  
-
-ipcMain.handle('openFile', async(event, args)=>{
-  if (fs.existsSync(args)){
-    handleAddRecent(args);
-    
-    return {fileContents:Buffer.from(fs.readFileSync(args)), filePath: args, status:"OK"};
-  }else{
-    return {fileContents:undefined, filePath:args, status:"NOTFOUND"};
-  }
-  
-  
-})
-
-ipcMain.handle('writeFile', async(event, args)=>{
-  const {filePath, toWrite} = args;
-  if (fs.existsSync(filePath)){
-    fs.writeFileSync(filePath, toWrite);
-    return "OK";
-  }else{
-    return "NOTFOUND"
-  }
-
-})
-
-ipcMain.handle('fileDialog:create', async()=>{
-  const fileDialog = await dialog.showSaveDialog({title:"Create new file"
-    ,filters:[{
-      name:'vault', extensions:['.vlt']
-    }]
-  });
-  if (!fileDialog.canceled){
-    const filePath = fileDialog.filePath;
-    // we expect this to error because the file doesn't exist yet, so this call will actually only cerate the file
-    handleAddRecent(filePath);
-    fs.open(filePath,'w+', (_)=>{
-    })
-    
-    return filePath;
-  }
-  return undefined;
-})
-
-
-
-
-ipcMain.handle('recents:get', ()=>{
-  const recentsPath = path.join(data_path , "/recents.json");
-    if (fs.existsSync(recentsPath)){
-      // read file 
-      const recentVaults = JSON.parse(fs.readFileSync(recentsPath).toString());
-      const existingVaults = recentVaults.filter((x:string)=>fs.existsSync(x))
-      // make sure that if for some reason the vaults were deleted in between sessions, then we are able to 
-      // update recents to exlude those vaults
-      if (existingVaults.length != existingVaults ){
-        
-        fs.writeFileSync(path.join(data_path + "/recents.json"), JSON.stringify(existingVaults));
-      }
-      return existingVaults;
-      
-    }else{
-      // create the file and return empty array
-      fs.writeFileSync(data_path + "/recents.json", "[]");
-      return [];
-    }
-})
-
-ipcMain.on('recents:add', (_,filepath)=>{
-    handleAddRecent(filepath);
-})
 
 ipcMain.on('updatePreference', (_,args)=>{
 
@@ -157,21 +75,4 @@ ipcMain.handle('clipboard:clear', ()=>{
 })
 
 
-const handleAddRecent = (filePath:string)=>{
-  let content:Array<string> = [];
-  if(!fs.existsSync(path.join(data_path,"/recents.json"))){
-    fs.writeFileSync(path.join(data_path,"/recents.json"), "[]");
-    content = [];
-  }else{
-    // ensure no duplicates are in the recents
-    content = JSON.parse(fs.readFileSync(data_path+"/recents.json").toString()).filter((x:string)=>x!==filePath)
-  }
-  // only allow 10 entries
-  if (content.length >= 10){
-    content.pop();
-  }
-  // only add file if we can't find it
-  content.unshift(filePath);
-  fs.writeFileSync(path.join(data_path+"/recents.json"), JSON.stringify(content));
-}
 
