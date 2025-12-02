b5321ee423fd13981f3eebf2678177fb282a3d94
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-02 15:48:09 +1100

    added encrypt and decrypt utility functions to abstract away talking to the crypto library

---
+++ main/crypto/commons.ts
@@ -0,0 +1,24 @@
+import * as crypto from 'node:crypto';
+
+
+
+export function encrypt(content:Buffer,key:Buffer){
+    
+    const iv = crypto.randomBytes(12);
+    const cipher = crypto.createCipheriv('aes-256-gcm',key,iv);
+    const encrypted = Buffer.concat([
+        cipher.update(content),
+        cipher.final()
+    ])
+    const tag = cipher.getAuthTag();
+    return {iv,encrypted, tag};
+}
+
+export function decrypt(content:Buffer, key:Buffer, tag:Buffer,iv:Buffer){
+    const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv);
+    decipher.setAuthTag(tag);
+    return Buffer.concat([
+        decipher.update(content),
+        decipher.final()
+    ])
+}
