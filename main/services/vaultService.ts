1712af63849f581d17553dbc1ed3a6c8b5c5e5bb
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-02 15:53:47 +1100

    implemented unlockVault

--- main/services/vaultService.ts
+++ main/services/vaultService.ts
@@ -1,5 +1,9 @@
 import EventEmitter from "events";
-import {validateKEK} from '../helpers/crypto/keyFunctions';
+import * as argon2 from 'argon2';
+import { encrypt, makeDEK } from "../crypto/commons";
+import ElectronStore from 'electron-store';
+import { preferenceStore } from "../helpers/store/preferencesStore";
+import { deriveKEK } from "../crypto/keyFunctions";
 interface EntryMetaData{
     createDate:Date,
     lastEditedDate:Date,
@@ -38,7 +42,7 @@ interface Vault {
     fileContents:Buffer
     isUnlocked:boolean,
     wrappedVK: Buffer,
-    kek:KEKParts | undefined, //KEK should be set to undefined when the vault is locked 
+    kek:Buffer | undefined, //KEK should be set to undefined when the vault is locked 
     entries: Array<Entry>,
     vaultMetadata: vaultMetaData
 }
@@ -47,6 +51,11 @@ interface Vault {
 class VaultService extends EventEmitter{
     private vault:Vault | undefined = undefined;
     private vaultInitialised = false;
+
+    constructor(){
+        super();
+
+    }
 
     setInitialVaultState(filePath:string, fileContents:Buffer){
         this.vault = {
@@ -66,32 +75,72 @@ class VaultService extends EventEmitter{
         this.vaultInitialised = true;
     }
 
-    unlockVault(password:string,){
-        const salt = this.vault.fileContents.subarray(0,16);
-        const hash = this.vault.fileContents.subarray(16,32)
+    async unlockVault(password:string){
+        const idx = this.vault.fileContents.findIndex((charcode)=>{
+            return charcode === 124
+        })
+        if (idx === -1){
+            throw new Error("CRITICAL ERROR could not find end of argon hash string, unable to verify password");
+        }
+        const hash = this.vault.fileContents.subarray(0,idx);
+        // incorrect password
+        if (!(await argon2.verify(hash.toString(), password))){
+            return {
+                entriesToDisplay: [],
+                status: "INVALID_PASSWORD"
+            };
+        }
+        const saltLength = preferenceStore.get('saltLength');
+        const salt = this.vault.fileContents.subarray(idx, idx+saltLength);
+        
+        this.vault.kek = await argon2.hash(password, {
+            timeCost: preferenceStore.get('timeCost'),
+            parallelism: preferenceStore.get('parallelism'),
+            memoryCost: preferenceStore.get('memoryCost'),
+            hashLength: 32,
+            salt: salt,
+            raw:true
+        });
 
-        // validateKEK(this.vault.fileContents, password);
+        this.deserialiseEntries();
 
+        return {
+            entriesToDisplay : this.getPaginatedEntries(1),
+            status: "OK"
+        };
     }
-
-
-
 
     closeVault(){
         this.vault = undefined; 
         this.vaultInitialised = false;
     }
 
-    getPaginatedEntries(pageNumber:number, pageLen:number){
+    lockVault(){
+        this.vault.kek = undefined;
+        this.vault.isUnlocked = false;
+    }
+
+    addEntry(title:string, username:string, password:string, notes:string, extraFields:Array<ExtraField> ){
+        const newDEK = makeDEK();
+        const encryptedPass = encrypt(Buffer.from(password), this.vault.kek);
+
+    }
+
+    getPaginatedEntries(pageNumber:number){
+        const pageLen = preferenceStore.get('entriesPerPage');
         const paginatedEntries = this.vault.entries.slice(pageNumber*pageLen, pageNumber*pageLen + pageLen);
         return{
-            paginatedEntries,
+            paginatedEntries
         }
     }
+
     searchEntries(title:string, username:string, notes:string){
         return this.vault.entries.filter((x)=>{
 
         })
+    }
+    private deserialiseEntries(){
+
     }
 }
 
