f93d94c72a84c817ea86a82a43f40fcb9052cc1b
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-04 09:56:49 +1100

    removed preference channels for the time being

--- main/preload.ts
+++ main/preload.ts
@@ -3,16 +3,13 @@ import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron'
 const handler = {
   openFilePicker: ()=>ipcRenderer.invoke('fileDialog:open'),
   openCreateFile: ()=>ipcRenderer.invoke('fileDialog:create'),
-  openFile: (filePath:string)=>ipcRenderer.invoke('openFile', filePath),
+  // openFile: (filePath:string)=>ipcRenderer.invoke('openFile', filePath),
+  openVault: (filePath:string)=>ipcRenderer.invoke('vault:open', filePath),
   handleHome: ()=>ipcRenderer.send('home'),
   getRecents: ()=>ipcRenderer.invoke('recents:get'),
   addRecent: (filePath:string)=>ipcRenderer.send('recents:add', filePath),
   writeFile: (filePath:string, toWrite:Buffer)=>ipcRenderer.invoke('writeFile', ({filePath, toWrite})),
   clearClipboard: ()=>ipcRenderer.invoke('clipboard:clear'),
-  getPreference: ()=>ipcRenderer.invoke('getPreferences'),
-  updatePreference: (prefName:string, newValue:any)=>ipcRenderer.invoke('updatePreference', ({prefName, newValue})),
-  addPreference: (prefName:string, newValue:any)=>ipcRenderer.invoke('addPreference', ({prefName, newValue})),
-  delete: (prefName:string)=>ipcRenderer.invoke('deletePreference', ({prefName})),
   vaultOpen:(cb:Function)=>{ipcRenderer.on('vault:open', (_, payload)=>cb(payload))}
 }
 
