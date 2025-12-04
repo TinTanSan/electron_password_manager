26cd8316fc4e754ba03f08c70b977ace8163f6fe
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-04 20:42:32 +1100

    vault:open handler now using openVault function of vautlService to call and initialise internal vault state rather than manually setting it from the handler

--- main/ipcHandlers/vaultIPCHandlers.ts
+++ main/ipcHandlers/vaultIPCHandlers.ts
@@ -4,22 +4,16 @@ import { openFile } from "./fileIPCHandlers";
 
 ipcMain.handle('vault:open', (_,filePath)=>{
     try{
-        let fileContents;
         if (typeof filePath === 'string'){
-            fileContents = openFile(filePath);
+            vaultService.openVault(filePath);
         }else{
             if (filePath.filePath){
-                fileContents = openFile(filePath.filePath)
+                vaultService.openVault(filePath.filePath);
             }else{
-                fileContents = openFile(filePath[0]);
+                vaultService.openVault(filePath[0]);
             }
         }
-        if (fileContents.status === "OK"){
-            vaultService.setInitialVaultState(filePath, fileContents.filecontents);
-            return {message:fileContents.length > 0? "PASS_SET":"SET_PASS"};
-        }else{
-            return {message:"NOT_OK"};
-        }
+        return {message:vaultService.vaultInitialised? "PASS_SET":"SET_PASS"};
     }catch(e:any){
         console.log('could not handle vault:open ipc channel, filepath given was not in the format expected, please give only filepath in args to the channel');
     }
@@ -35,7 +29,7 @@ ipcMain.on('vault:close', ()=>{
     vaultService.closeVault();
 })
 
-ipcMain.on('vault:setPass', async (_,password)=>{
+ipcMain.handle('vault:setPass', async (_,password)=>{
     return await vaultService.setMasterPassword(password);
 })
 
