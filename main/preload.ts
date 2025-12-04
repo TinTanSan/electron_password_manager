dc6818f96748836ab48aceb855e7f200ac0fc65e
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-04 18:31:10 +1100

    removed old vaultOpen handler and added a new handler for setting vault's master password

--- main/preload.ts
+++ main/preload.ts
@@ -5,12 +5,14 @@ const handler = {
   openCreateFile: ()=>ipcRenderer.invoke('fileDialog:create'),
   // openFile: (filePath:string)=>ipcRenderer.invoke('openFile', filePath),
   openVault: (filePath:string)=>ipcRenderer.invoke('vault:open', filePath),
+  setVaultMasterPass: (password:string)=>ipcRenderer.invoke('vault:setPass', password)
+  
+  
   handleHome: ()=>ipcRenderer.send('home'),
   getRecents: ()=>ipcRenderer.invoke('recents:get'),
   addRecent: (filePath:string)=>ipcRenderer.send('recents:add', filePath),
   writeFile: (filePath:string, toWrite:Buffer)=>ipcRenderer.invoke('writeFile', ({filePath, toWrite})),
   clearClipboard: ()=>ipcRenderer.invoke('clipboard:clear'),
-  vaultOpen:(cb:Function)=>{ipcRenderer.on('vault:open', (_, payload)=>cb(payload))}
 }
 
 
