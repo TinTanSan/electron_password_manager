9e288b5199946fca0eb5b3497bd3bf54f7e64276
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-10 16:57:30 +1100

    moved parsing rules into their own file to be used with serialisers

---
+++ main/helpers/serialisation/rules.ts
@@ -0,0 +1,47 @@
+export const entryMDVersionConstituents:Record<string, Array<Array<string>>>= {    
+    '1.0.0':[
+        ['split'          , '$'],
+        ["version"        , 'string'],
+        ["createDate"     , 'date'],
+        ['lastEditDate'   , 'date'],
+        ["lastRotateDate" , 'date'],
+        ["uuid"           , 'string'],
+    ]
+    // example : 1.0.0.0$25-06-2025T00:00:00Z000$25-06-2025T00:00:00Z000$25-06-2025T00:00:00Z000$ab12-cd34-ef56-gh78
+
+}
+
+export const vaultMDVersionConstituents:Record<string, Array<Array<string>>> = {
+    '1.0.0':[
+        ['split',"$"],
+        ["version"        , 'string'],
+        ["createDate"     , 'date'],
+        ['lastEditDate'   , 'date'],
+    ]
+}
+
+
+export const entryConstituents = {
+    '1.0.0':[
+        ['split'      , "|"],
+        ['entryMD'    , 'entryMD'],
+        ['username'   , "string"],
+        ['password'   , 'buffer'],
+        ['passHash'   , 'buffer'],
+        ['notes'      , "string"],
+        ['isFavourite', "isFavToBool"],
+        ['entryGroup', 'string'],
+        ['extraFields', 'extraFields']
+    ]
+    ,
+
+}
+
+export const vaultConstituents  = {
+    '1.0.0' : [
+        ['split',"|"],
+        ['vaultMetadata', "vaultMD"],
+        ['entries','entries'],
+        ['entryGroups', 'entryGroup']
+    ]
+}
