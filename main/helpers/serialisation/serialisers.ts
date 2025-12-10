5162bf3c3793480c77a563d5dadbfde0842a9780
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-10 20:18:41 +1100

    created serialisers for vault and entry

--- main/helpers/serialisation/serialisers.ts
+++ main/helpers/serialisation/serialisers.ts
@@ -1,5 +1,5 @@
-import { EntryMetaData, vaultMetaData } from "../../services/vaultService";
-import { entryMDVersionConstituents, vaultMDVersionConstituents } from "./rules";
+import { Entry, EntryMetaData, Vault, vaultMetaData } from "../../services/vaultService";
+import { entryConstituents, entryMDVersionConstituents, vaultMDVersionConstituents } from "./rules";
 
 export const serialisers = {
     'date': (d:Date)=>d.toISOString(),
@@ -10,7 +10,7 @@ export const serialisers = {
     'entryMD': (entryMetadata:EntryMetaData)=>{
         const consitituents = entryMDVersionConstituents[entryMetadata.version];
         let res = "";
-        const joiner = entryMDVersionConstituents[0][1];
+        const joiner = consitituents[0][1];
         for (let constituent of consitituents){
             if (constituent[0] === "split") continue;   
             res += serialisers[constituent[1]](entryMetadata[constituent[0]]) + joiner;
@@ -19,13 +19,30 @@ export const serialisers = {
     },
 
     'vaultMD': (md:vaultMetaData)=>{
-            // const split = md.split(vaultMDVersionConstituents[version][0][1]);
-            let ret:any;
-            const parsersToUse = vaultMDVersionConstituents[md.version];
-            const joiner = vaultMDVersionConstituents[0][1];
-            for (let i = 1; i<parsersToUse.length; i++){
-                ret.parsersToUse[i][0] = serialisers[parsersToUse[i][1]](md[parsersToUse[i][0]]) + joiner ;
-            }
-            return ret;
-        },
+        // const split = md.split(vaultMDVersionConstituents[version][0][1]);
+        let ret= "";
+        const serialiserToUse = vaultMDVersionConstituents[md.version];
+        const joiner = serialiserToUse[0][1];
+        for (let i = 1; i<serialiserToUse.length; i++){
+            ret += serialisers[serialiserToUse[i][1]](md[serialiserToUse[i][0]]) + joiner ;
+        }
+        return ret;
+    },
+
+    'entry' : (entry:Entry)=>{
+        const serialiserToUse = entryConstituents[entry.metadata.version];
+        const joiner = serialiserToUse[0][1];
+        let ret = "";
+        for (let i = 1; i<serialiserToUse.length; i++){
+            ret += serialiserToUse[serialiserToUse[i][1]](entry[serialiserToUse[i][0]]) + joiner;
+        }
+    },
+    'vault': (vault:Vault)=>{
+        const serialiserToUse = entryConstituents[vault.vaultMetadata.version];
+        const joiner = serialiserToUse[0][1];
+        let ret = "";
+        for (let i = 1; i<serialiserToUse.length; i++){
+            ret += serialiserToUse[serialiserToUse[i][1]](vault[serialiserToUse[i][0]]) + joiner;
+        }
+    }
 }
