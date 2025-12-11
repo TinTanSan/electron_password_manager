6b6946810d57f49ae7d3262f6397dbc1a5c06344
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-11 14:26:20 +1100

    updated entry parser and entryMD parser to properly parse serialised entries

--- main/helpers/serialisation/parsers.ts
+++ main/helpers/serialisation/parsers.ts
@@ -1,37 +1,47 @@
 import { parse } from "path";
 import { entryConstituents, entryMDVersionConstituents, vaultConstituents, vaultMDVersionConstituents } from "./rules";
 import { serialisers } from "./serialisers";
-import { ExtraField } from "../../services/vaultService";
+import { EntryMetaData, ExtraField } from "../../services/vaultService";
+import { assert } from "console";
 
 
 export const parsers = {
     // primitive parsers
-    'string':(s:string|Buffer):string=>s.toString(),
+    'string':(s:string|Buffer):string=>typeof s === 'string'? s : s.toString(),
     'buffer':(s:string):Buffer=>Buffer.from(s),
     'date': (d:string):Date=>new Date(d),
     'isFavToBool': (str:string):boolean=>str==="1",
 
     // constituent parsers
-    'entryMD': (md:string, version: string):any | undefined=>{
-        const split = md.split(entryMDVersionConstituents[version][0][1]);
-        let ret:any;
+    'entryMD': (md:string):any | undefined=>{
+        const version = md.split("$")[0];
+        const split = md.split(entryMDVersionConstituents[version][0][1]).slice(0,-1);
+        let ret:EntryMetaData = {
+            version: '',
+            createDate: new Date(),
+            uuid: '',
+            lastEditDate: new Date(),
+            lastRotateDate: new Date(),
+        };
         const parsersToUse = entryMDVersionConstituents[version];
-        
+        assert(split.length === parsersToUse.length-1, 'split did not split entry metadata to the number of items expected, expected '+parsersToUse.length+" but got "+split.length);
         for (let i = 1; i<parsersToUse.length; i++){
-            ret.parsersToUse[i][0] = parsers[parsersToUse[i][1]](split[i-1]);
+            console.log(parsersToUse[i])
+            console.log('using it on str val: ', parsers[parsersToUse[i][1]](split[i-1]))
+            ret[parsersToUse[i][0]] = parsers[parsersToUse[i][1]](split[i-1]);
         }
         return ret;
     },
     // all extrafields
     'extraFields': (efs:string)=>{
-        return efs.split('`').map((ef:string)=>{
+        return efs ? efs.split('`').map((ef:string)=>{
             const split = ef.split("_");
             return {
                 name: split[0], 
                 data: parsers.buffer(split[1]), 
                 isProtected: parsers.isFavToBool(split[2])
             };
-        })
+        }) : []
     },
 
     'vaultMD': (md:String, version:string)=>{
@@ -62,15 +72,10 @@ export const parsers = {
         const version = entryStr.substring(0,entryStr.indexOf("$"));
         const constituents = entryConstituents[version];
         const split = entryStr.split(constituents[0][1]);
-        let counter = 1;
         let res = {}
-        for(let str of split){
-            try {
-                res[constituents[counter]] = parsers[constituents[counter][1]](str)    
-            } catch (error) {
-                throw new Error('Error occured whilst parsing entry: ',error);
-                
-            }
+        for(let i = 1; i< constituents.length; i++){
+            const constituent = constituents[i];
+            res[constituent[0]] = parsers[constituent[1]](split[i-1]);
         }
         return res;
     },
