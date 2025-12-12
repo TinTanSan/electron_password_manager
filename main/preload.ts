816b3e0c6f43363ced5b150cae11af1bdface048
TinTanSan <tirthvpatel2003@gmail.com>
2025-12-12 11:59:27 +1100

    removed ipc channels for write and open file

--- main/preload.ts
+++ main/preload.ts
@@ -3,15 +3,13 @@ import { contextBridge, ipcRenderer } from 'electron'
 const handler = {
   openFilePicker: ()=>ipcRenderer.invoke('fileDialog:open'),
   openCreateFile: ()=>ipcRenderer.invoke('fileDialog:create'),
-  // openFile: (filePath:string)=>ipcRenderer.invoke('openFile', filePath),
   openVault: (filePath:string)=>ipcRenderer.invoke('vault:open', filePath),
   setVaultMasterPass: (password:string)=>ipcRenderer.invoke('vault:setPass', password),
   unlockVault: (password:string)=>ipcRenderer.invoke('vault:unlock', password),
-
   handleHome: ()=>ipcRenderer.send('home'),
   getRecents: ()=>ipcRenderer.invoke('recents:get'),
   addRecent: (filePath:string)=>ipcRenderer.send('recents:add', filePath),
-  writeFile: (filePath:string, toWrite:Buffer)=>ipcRenderer.invoke('writeFile', ({filePath, toWrite})),
+  // writeFile: (filePath:string, toWrite:Buffer)=>ipcRenderer.invoke('writeFile', ({filePath, toWrite})), to be replaced with vault update
   clearClipboard: ()=>ipcRenderer.invoke('clipboard:clear'),
 }
 
