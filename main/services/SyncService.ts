de5dbfb21ba145c15a9cae0fd55f9eee917bf0b0
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-08 19:04:58 +1100

    moved delaying outside of the  writing if statement to ensure that the sync loop always sleeps for at least 5 seconds

--- main/services/SyncService.ts
+++ main/services/SyncService.ts
@@ -7,30 +7,39 @@ export class SyncService{
     constructor(filePath:string){
         this.filePath = filePath;
         this.stopSync = false;
+        this.writeBuffer = Buffer.from("");
+        this.startSyncLoop();
     }
 
     async startSyncLoop(){
         let result = "";
         while (!this.stopSync){
             if(this.writeBuffer.length  > 0){
-                result = writeToFileAsync({filePath:this.filePath, toWrite: this.writeBuffer})
+                console.log('writing')
+                result = await writeToFileAsync({filePath:this.filePath, toWrite: this.writeBuffer})
                 if (result !== "OK"){
                     this.stopSync = true;
                     throw new Error("Failed backup in syncLoop, reason: "+result);
                 }
                 this.writeBuffer = Buffer.from("");
-                await this.delay(5000);
+                console.log('waiting')
+                
             }
+            await this.delay(5000);
+            console.log('delaying')
         }
+        console.log('sync loop stopped')
     }
     private delay(ms:number){
         return new Promise(resolve => setTimeout(resolve, ms));
     }
     updateBuffer(content:Buffer){
         this.writeBuffer = content;
+        console.log('updated buffer', content)
     }
     stopSyncLoop(){
         this.stopSync = true;
+        console.log('stopping syncloop')
     }
 
 }
