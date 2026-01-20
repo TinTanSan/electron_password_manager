8b0cfdda5ae0adb50e0233c95dacf8b58a1fd0c8
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-20 17:22:14 +1100

    removed unused VaultType

--- renderer/interfaces/Vault.ts
+++ renderer/interfaces/Vault.ts
@@ -1,17 +1,6 @@
-import { decrypt, encrypt } from "../utils/commons";
-import { makeNewDEK } from "../utils/keyFunctions";
-import { Entry } from "./Entry"
-type PartialWithRequired<T, K extends keyof T> = Partial<T> & Pick<T, K>;
 
-export type VaultType = {
-    filePath:string, 
-    fileContents:Buffer
-    isUnlocked:boolean,
-    wrappedVK: Buffer,
-    kek:KEKParts | undefined, //KEK should be set to undefined when the vault is locked 
-    entries: Array<Entry>,
-    vaultMetadata: vaultMetaData
-}
+import { Entry } from "./Entry"
+
 
 type vaultMetaData = {
     lastRotateDate: Date,   
