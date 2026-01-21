203e1556dc0c1162eead0a9d1e58c7c9c4d49ba0
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-21 19:43:04 +1100

    minor css changes

--- renderer/pages/groups.tsx
+++ renderer/pages/groups.tsx
@@ -6,14 +6,14 @@ import { useRouter } from 'next/navigation';
 export default function GroupsPage() {
     const {vault, setVault} = useContext(VaultContext);
   return (
-    <div className='flex w-screen h-screen overflow-hidden bg-gradient-to-b from-base-200 to-base-300'>
+    <div className='flex w-screen h-screen overflow-hidden bg-linear-to-b from-base-200 to-base-300'>
         <title>Groups</title>
         <Sidebar />
         {/* main content */}
         <div className='flex w-full h-full p-2'>
             <div className='flex flex-col w-full h-full gap-2'>
                 {vault?.entryGroups.map((group, i)=>
-                    <div key={i} className='flex border-2 h-12 w-full rounded-lg border-base-300 bg-white px-2 items-center text-md font-[500] gap-5'>
+                    <div key={i} className='flex border-2 h-12 w-full rounded-lg border-base-300 bg-white px-2 items-center text-md font-medium gap-5'>
                         <div className='flex w-full'>
                             {group.groupName}
                         </div>
