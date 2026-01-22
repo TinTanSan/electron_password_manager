3e47bcfb0a7f8717a059d5540650f0132254ef01
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-22 19:13:36 +1100

    added constants which were deleted before

--- renderer/utils/commons.ts
+++ renderer/utils/commons.ts
@@ -14,3 +14,7 @@ export function isStrongPassword(password:string):string{
     }
     return passwordMessage.length > 0? "The password "+passwordMessage.join(", ") : "";
 }
+export const lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+export const upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+export const digits = ["0","1","2","3","4","5","6","7","8","9"];
+export const asciiSafeSpecialChars = ["!","\"","#","$","%","&","'","(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
