7ea585a231177cea87ed39355844014ffb6f464e
TinTanSan <tirthvpatel2003@gmail.com>
2026-02-08 19:54:52 +1100

    added a default state so that vault can easily be set to a state when user vaults are closed

--- renderer/contexts/vaultContext.tsx
+++ renderer/contexts/vaultContext.tsx
@@ -7,7 +7,20 @@ type VaultCxtType = {
     setVault: React.Dispatch<React.SetStateAction<Vault | undefined>>
 }
 
+export const defaultVaultState:Vault = {
+    filePath: "",
+    isUnlocked: false,
+    entries: [],
+    entryGroups: [],
+    vaultMetadata:{
+        lastRotateDate: new Date(),
+        createDate: new Date(),
+        lastEditDate: new Date(),
+        version: "0.1.0"
+    }
+}
+
 export const VaultContext = createContext<VaultCxtType>({
         vault: undefined,
         setVault: (_:Vault) => { throw new Error("setVault called outside provider") }
-    })
+})
