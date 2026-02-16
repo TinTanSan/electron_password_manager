f98b3d17d298d074a3aef67de0e6a9f4b3c5bd89
TinTanSan <tirthvpatel2003@gmail.com>
2026-02-17 09:11:55 +1100

    created a comparison function to handle comparing states of an Entry

--- renderer/utils/commons.ts
+++ renderer/utils/commons.ts
@@ -1,3 +1,6 @@
+
+import { Entry } from "../interfaces/Entry";
+
 export function isStrongPassword(password:string):string{
     let passwordMessage = []
     if (password.length <8){
@@ -14,6 +17,35 @@ export function isStrongPassword(password:string):string{
     }
     return passwordMessage.length > 0? "The password "+passwordMessage.join(", ") : "";
 }
+
+function cmpObj(left:any, right:any){
+    for (let k in left){
+        // recurse
+        if (typeof left[k] === 'object' && right[k]){
+            cmpObj(left[k], right[k])
+        }
+        if (!right[k] || right[k] !== left[k]){
+            console.log("not eq for param", k)
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+export function cmpEntry(left:Entry, right:Entry){
+    for (let key in left){
+        if (!(key in right)) return false;
+        if (typeof left[key] === "object"){
+            if (!cmpObj(left[key], right[key]))return false
+        }
+        if (right[key] !== left[key]) return false
+    }
+    return true;
+
+    // return isDeepStrictEqual(left, right)
+}
+
 export const lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 export const upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 export const digits = ["0","1","2","3","4","5","6","7","8","9"];
