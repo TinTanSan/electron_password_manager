c3945108638ff1d52e0b6074f764b2895f29fa52
TinTanSan <tirthvpatel2003@gmail.com>
2026-02-25 11:51:50 +1100

    removed unused imports and code

--- renderer/components/banner.tsx
+++ renderer/components/banner.tsx
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { BannerDetails } from '../interfaces/Banner'
-import { BannerContext } from '../contexts/bannerContext';
-import Image from 'next/image';
+import React, { useContext,} from 'react'
+import { BannerDetails } from '@interfaces/Banner'
+import { BannerContext } from '@contexts/bannerContext';
+
 
 export default function Banner({bannerDetails}:{bannerDetails:BannerDetails}) {
     const banners = useContext(BannerContext);
@@ -22,13 +22,6 @@ export default function Banner({bannerDetails}:{bannerDetails:BannerDetails}) {
 
     
   return (
-    // <div className={`flex relative flex-row animate-slide-in border-2 px-2 justify-between items-center overflow-hidden rounded-lg h-fit ${handleColour()} w-full`}>
-    
-    //     <div className={`flex absolute top-0 h-1 opacity-100 saturate-150 rounded-full brightness-90 animate-[shrink_2950ms_linear_forwards] duration-1000 ${handleStripeColour()}`} />
-    //     <Image src={'/images/'+bannerDetails.type+".svg"} alt={bannerDetails.type} width={20} height={20} className='flex h-auto fill-success-content'/>
-    //     <div className='flex w-full h-full opacity-100 items-center text-lg p-2 cursor-default '>{bannerDetails.text}</div>
-    //     <button className='flex text-2xl w-fit h-fit' onClick={()=>{banners.setBanners(prev=>prev.filter(x=>x.id !== bannerDetails.id))}}>&#x2716;</button>
-    // </div>
     <div className={`flex flex-row relative animate-slide-in  border-r-2 border-t-2 border-b-2 border-base-300 gap-1 justify-between items-center overflow-hidden rounded-lg h-14 max-h-fit bg-white w-full`}>
         <div className={`flex w-2 h-full ${handleColour()}`} />
         <div className='flex w-full h-full items-center py-2'>{ bannerDetails.text }</div>
