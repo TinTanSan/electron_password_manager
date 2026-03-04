d8f4382f9067af8ff3aef89a9e7b29bd9e81daa9
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-04 13:23:59 +1100

    added inputs for settings and created some placeholder font size pickers

--- renderer/pages/settings.tsx
+++ renderer/pages/settings.tsx
@@ -1,5 +1,6 @@
 import Sidebar from '@components/Sidebar';
-import { minMaxValuesForPreferences, PreferenceContext } from '@contexts/preferencesContext';
+import Slider from '@components/Slider';
+import { PreferenceContext, preferenceInputMapper } from '@contexts/preferencesContext';
 import { VaultContext } from '@contexts/vaultContext'
 import { useRouter } from 'next/router';
 import React, { useContext, useEffect, useState } from 'react'
@@ -14,14 +15,16 @@ export default function Settings() {
     // which will have 3-5 predefined values they could be
     const handlePreferenceChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
         const v = Number(e.target.value)
-
-        if (!Number.isNaN(v) && Object.keys(minMaxValuesForPreferences).includes(e.target.id)){
-            const [min, max] = minMaxValuesForPreferences[e.target.id];
+        console.log(v)
+        if (!Number.isNaN(e.target.value)){
+            const {min, max} = preferenceInputMapper[e.target.id]
             if (v < min){
                 e.target.value = min;
             }else if (v > max){
                 e.target.value = max;
             }
+        }else if (e.target.value === ""){
+
         }
 
         setPreference(prev=>({...prev, [e.target.id]:e.target.value}))
@@ -38,7 +41,7 @@ export default function Settings() {
     },[])
 
     return (
-    <div className='flex gap-5 w-scren h-screen bg-base-200 p-2 '> 
+    <div className='flex gap-5 w-scren h-screen bg-base-200 p-2 text-neutral'> 
         <Sidebar />
         <div className='flex flex-col w-full h-full'>
             <div className='flex flex-col w-full h-fit'>
@@ -49,17 +52,73 @@ export default function Settings() {
                     
                 </div>
             </div>
-            <div className='flex w-full h-fit flex-col'>
+            <div id='preferences' className='flex w-full h-fit flex-col gap-2'>
                 <div className='flex flex-col text-xl justify-center w-full'>
                     Preferences
                 </div>
-                <div className='flex w-full h-full bg-base-100 rounded-lg flex-col p-2'>    
-                    <div className='flex flex-col'>
-                        <label>Vault Lock Time Out (minutes)</label>
-                        <i className='text-xs'>minimum: {minMaxValuesForPreferences['vaultLockTimeOut'][0]}, maximum: {minMaxValuesForPreferences['vaultLockTimeOut'][1]}</i>
-                        <input className='flex w-30 h-8 border-2 ' id='vaultLockTimeOut' onChange={handlePreferenceChange} />
+                <div className='flex w-full h-full  rounded-lg flex-wrap p-2 gap-5 justify-between'>    
+                    {
+                        Object.keys(preferenceInputMapper).map((key)=>
+                        {
+                            const inputSettings = preferenceInputMapper[key];
+                            return (
+                                <div className='flex flex-col w-[45vw] border-2 shrink-0 border-base-300 p-2 bg-base-100 rounded-lg gap-2'>
+                                    <div className='flex flex-col'>
+                                        <label>{inputSettings.label}</label>
+                                        <i className='text-xs'>minimum: {inputSettings.min}, maximum: {inputSettings.max}</i>
+                                    </div>
+                                    <input value={Number.isNaN(preference[key])?"": preference[key]} className='flex w-30 h-8 border-2 px-1 rounded-lg border-base-300' id={key} onChange={handlePreferenceChange} />
+                                </div>
+                            )
+                        }
+                        )
+                    }
+                </div>
+                <div className='flex flex-row gap-1 rounded-lg w-full h-fit p-2'>
+                    
+                    <div className='flex flex-col w-full h-fit bg-base-100 border-2 rounded-lg p-2 border-base-300'>
+                        <div>
+                            Font sizes
+                        </div>
+                        <div className='flex flex-row gap-2 h-28 shrink-0 w-full'>
+                            <div className='flex flex-row w-full h-full p-2 items-center rounded-lg gap-5 text-base-content'>
+                                <div className='flex flex-col cursor-pointer hover:bg-base-300 hover:h-25 h-24 aspect-square shrink-0 p-1 items-center justify-between border-2 border-base-300 rounded-md text-sm'>
+                                    <p>Small</p>
+                                    <p>aA</p>
+                                    <p>14 px</p>
+                                </div>  
+                                <div className='flex flex-col cursor-pointer hover:bg-base-300 hover:h-25 h-24 aspect-square shrink-0 p-1 border-2 border-base-300 justify-between items-center rounded-lg text-md'>
+                                    <p>Medium</p>
+                                    <p>aA</p>
+                                    <p>16 px</p>
+                                </div>  
+                                <div className='flex flex-col cursor-pointer hover:bg-base-300 hover:h-25 h-24 aspect-square shrink-0  p-1 border-2 items-center justify-between border-base-300 rounded-lg text-lg'>
+                                    <p>Large</p>
+                                    <p>aA</p>
+                                    <p>18 px</p>
+                                </div>  
+                            </div>
+                        </div>
+
+                        <div className='flex flex-col gap-2 h-fit w-full'>
+                            <div>Preview</div>
+                            <div className='flex flex-col w-full h-full'>
+                                <div className='flex  w-full items-center'>Titles Look like this</div>
+                                <div className='flex  w-full items-center'>Sub headings</div>
+                                <div className='flex  w-full items-center'>main content</div>
+                                <div className='flex  w-full items-center'>subnotes</div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className='flex flex-col w-full  h-fit rounded-lg'>
+                        <div>Line spacing</div>
+                        <div className='flex flex-row w-full h-28 bg-base-100 border-base-300 border-2 rounded-lg'>
+
+                        </div>
                     </div>
                 </div>
+                
             </div>
         </div>
     </div>
