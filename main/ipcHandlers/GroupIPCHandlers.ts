051edaa650e010b2cabe052cbcebb6ec239930b2
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-07 18:43:25 +1100

    now referencing new entry based ipc channels

--- main/ipcHandlers/GroupIPCHandlers.ts
+++ main/ipcHandlers/GroupIPCHandlers.ts
@@ -2,13 +2,13 @@ import { ipcMain } from "electron";
 import { vaultService } from "../services/vaultService";
 import { IPCResponse } from "../interfaces/IPCCHannelInterface";
 
-ipcMain.handle("vault:addEntryToGroup", async (_, entryUUID, groupName)=>vaultService.addEntryToGroup(entryUUID, groupName))
+ipcMain.handle("entry:addEntryToGroup", async (_, entryUUID, groupName)=>vaultService.addEntryToGroup(entryUUID, groupName))
 
-ipcMain.handle('vault:removeEntryFromGroup', async(_, entryUUID:string)=>vaultService.removeEntryFromGroup(entryUUID))
-ipcMain.handle("vault:deleteGroup", async(_, groupName:string)=>vaultService.deleteGroup(groupName))
+ipcMain.handle('entry:removeEntryFromGroup', async(_, entryUUID:string)=>vaultService.removeEntryFromGroup(entryUUID))
+ipcMain.handle("entry:deleteGroup", async(_, groupName:string)=>vaultService.deleteGroup(groupName))
 
 
-ipcMain.handle('vault:getGroups', ():IPCResponse<Array<string>>=>{
+ipcMain.handle('entry:getGroups', ():IPCResponse<Array<string>>=>{
     return {
         status: "OK",
         response: vaultService.getAllGroups()
@@ -16,7 +16,7 @@ ipcMain.handle('vault:getGroups', ():IPCResponse<Array<string>>=>{
 
 })
 
-ipcMain.handle('vault:searchGroups', (_,searchString:string):IPCResponse<Array<string>>=>{
+ipcMain.handle('entry:searchGroups', (_,searchString:string):IPCResponse<Array<string>>=>{
     return {
         status: "OK",
         response: vaultService.searchGroups(searchString)
