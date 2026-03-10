461e722740bcd4de6b64214d1ca503b28c94d704
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-10 15:39:54 +1100

    type for newValue is now any to accomodate either string or number

--- main/ipcChannels/preferenceIPCChannels.ts
+++ main/ipcChannels/preferenceIPCChannels.ts
@@ -1,7 +1,7 @@
 import { ipcRenderer } from "electron";
 
 export const preferenceIPCChannels = {
-  setPreference: (preferenceName:string, newValue:string)=>ipcRenderer.invoke('preference:set', preferenceName, newValue),
+  setPreference: (preferenceName:string, newValue:any)=>ipcRenderer.invoke('preference:set', preferenceName, newValue),
   getAllPreferences: ()=>ipcRenderer.invoke('preference:getAll'),
   getPreference: (preferenceName:string)=>ipcRenderer.invoke('preference:get', preferenceName)
 };
