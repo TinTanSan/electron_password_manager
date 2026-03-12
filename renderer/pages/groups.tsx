b8428c7ba45f0fb26484c4baa49dcba3783d6aeb
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-12 18:08:25 +1100

    added useeffect to fetch all groups on group page load

--- renderer/pages/groups.tsx
+++ renderer/pages/groups.tsx
@@ -5,25 +5,33 @@ import { useRouter } from 'next/navigation';
 
 export default function GroupsPage() {
     const {vault, setVault} = useContext(VaultContext);
-  return (
-    <div className='flex w-screen h-screen overflow-hidden bg-linear-to-b from-base-200 to-base-300'>
-        <title>Groups</title>
-        <Sidebar />
-        {/* main content */}
-        <div className='flex w-full h-full p-2'>
-            <div className='flex flex-col w-full h-full gap-2'>
-                {vault?.entryGroups.map((group, i)=>
-                    <div key={i} className='flex border-2 h-12 w-full rounded-lg border-base-300 bg-white px-2 items-center text-md font-medium gap-5'>
-                        <div className='flex w-full'>
-                            {group.groupName}
+    useEffect(()=>{
+        if(vault.filePath){
+            window.entryIPC.getGroups().then((response)=>{
+                setVault(prev=>({...prev, entryGroups: response.response}))
+            })
+        }
+    },[])
+
+    return (
+        <div className='flex w-screen h-screen overflow-hidden bg-linear-to-b from-base-200 to-base-300'>
+            <title>Groups</title>
+            <Sidebar />
+            {/* main content */}
+            <div className='flex w-full h-full p-2'>
+                <div className='flex flex-col w-full h-full gap-2'>
+                    {vault?.entryGroups.map((group, i)=>
+                        <div key={i} className='flex border-2 h-12 w-full rounded-lg border-base-300 bg-white px-2 items-center text-normal gap-5'>
+                            <div className='flex w-full text-subheading'>
+                                {group.groupName}
+                            </div>
+                            <p className='flex w-40 justify-end text-nowrap'>
+                                {group.entries.length} Entries
+                            </p>
                         </div>
-                        <p className='flex w-40 justify-end text-nowrap font-normal text-md'>
-                            {group.entries.length} Entries
-                        </p>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
