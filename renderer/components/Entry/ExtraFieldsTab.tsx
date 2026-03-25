ca9ff4f083765b7c6ebd9ff7097a41812d9edd7b
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-25 18:19:57 +1100

    minor changes

--- renderer/components/Entry/ExtraFieldsTab.tsx
+++ renderer/components/Entry/ExtraFieldsTab.tsx
@@ -63,14 +63,10 @@ export default function ExtraFieldsTab({entry, setEntry}:props) {
                 <input type="text" placeholder='search for an extra field' className='flex w-full h-8 px-1 rounded-lg border-2 border-base-300 focus:border-primary outline-none' />
             </div>
             <div className='flex text-sm flex-col p-1 rounded-lg w-full h-full overflow-y-auto gap-2 border bg-base-200 border-base-300'>
-                {/* <ExtraFieldComponent extraField={{isProtected:false, name:"Test", data:Buffer.from("hello")}} entry={entry} onDelete={handleDeleteExtraField} />
-                <ExtraFieldComponent extraField={{isProtected:false, name:"Test", data:Buffer.from("hello")}} entry={entry} onDelete={handleDeleteExtraField} />
-                <ExtraFieldComponent extraField={{isProtected:false, name:"Test", data:Buffer.from("hello")}} entry={entry} onDelete={handleDeleteExtraField} />
-                <ExtraFieldComponent extraField={{isProtected:false, name:"Test", data:Buffer.from("hello")}} entry={entry} onDelete={handleDeleteExtraField} /> */}
-                {entry.extraFields.map(ef=><ExtraFieldComponent extraField={ef} entry={entry} onDelete={handleDeleteExtraField} />)}
+                {entry.extraFields.map(ef=><ExtraFieldComponent key={ef.name} extraField={ef} entry={entry} onDelete={handleDeleteExtraField} />)}
             </div>
             {/* add extrafield form */}
-            <div className='flex flex-col w-full h-1/3 grow-0  shrink-0 border-2 border-base-300 bg-base-200 rounded-lg p-1'>
+            <div className='flex flex-col w-full h-1/3 grow-0  shrink-0 rounded-lg p-1'>
                 <div className='flex w-full items-center justify-end relative h-10'>
                     <div className='flex items-center w-full justify-center'>
                         Add another extra Field
@@ -79,11 +75,11 @@ export default function ExtraFieldsTab({entry, setEntry}:props) {
                 <form onSubmit={handleAddExtraField} className='flex flex-col h-full w-full justify-between'>
                     <div className='flex gap-2 w-full h-fit'>
                         <label className='flex w-12' >Name</label>
-                        <input type="text" id='name' onChange={handleChange} value={newExtraField.name} className='flex border-2 border-base-300 outline-none focus:border-primary px-1 bg-base-100 rounded-lg w-full' />
+                        <input type="text" id='name' onChange={handleChange} value={newExtraField.name} className='flex border-2 border-base-300 outline-none focus:border-base-contentb px-1 bg-base-100 rounded-lg w-full' />
                     </div>
                     <div className='flex gap-2 w-full h-fit'>
                         <label className='flex w-12' >Data</label>
-                        <input type="text" id="data"  onChange={handleChange} value={newExtraField.data.toString()} className='flex border-2 border-base-300 outline-none focus:border-primary px-1 bg-base-100 rounded-lg w-full' />
+                        <input type="text" id="data"  onChange={handleChange} value={newExtraField.data.toString()} className='flex border-2 border-base-300 outline-none focus:border-base-content px-1 bg-base-100 rounded-lg w-full' />
                     </div>
                     <div className='flex w-full h-fit gap-2 items-center'>
                         <p>Encrypt field?</p>
