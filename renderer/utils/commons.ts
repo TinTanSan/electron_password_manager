72636dbda68fe9e48c8b3c251ef20cdf594545c9
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-25 18:21:59 +1100

    added the IPCResponse type to the renderer process so that it can be used to guage types better for responses

--- renderer/utils/commons.ts
+++ renderer/utils/commons.ts
@@ -44,7 +44,11 @@ export function cmpEntry(left:Entry, right:Entry){
 
     // return isDeepStrictEqual(left, right)
 }
-
+export type IPCResponse<T> = {
+    status:"OK" | "CLIENT_ERROR" | "INTERNAL_ERROR",
+    message?:string,
+    response:T
+}
 export const lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 export const upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 export const digits = ["0","1","2","3","4","5","6","7","8","9"];
