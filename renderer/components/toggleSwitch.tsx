4d09dbca3bc480ac2bd663f99898e65f7237eb32
TinTanSan <tirthvpatel2003@gmail.com>
2026-03-25 17:03:30 +1100

    Refactor: Changed border colour

--- renderer/components/toggleSwitch.tsx
+++ renderer/components/toggleSwitch.tsx
@@ -7,8 +7,8 @@ type props = {
 }
 export default function ToggleSwitch({value, setValue}:props) {
   return (
-    <div className='flex w-10 h-5 border-2 rounded-full overflow-hidden justify-start relative' onClick={()=>{setValue(!value)}}>
-        <div className={`flex ${value?  "w-full" : "w-0"} h-full bg-neutral transition-all duration-300 absolute left-0 rounded-lg`} />
+    <div className='flex w-10 h-5 border-2 border-base-content rounded-full overflow-hidden justify-start relative' onClick={()=>{setValue(!value)}}>
+        <div className={`flex ${value?  "w-full" : "w-0"} h-full bg-base-content transition-all duration-300 absolute left-0 rounded-lg`} />
         <div className={`flex  w-4 shrink-0 h-4 rounded-full border-2 z-10 bg-white absolute  ${value?"left-[calc(100%-16px)] duration-300 ":" left-0  duration-200"} transition-all`} />
     </div>
   )
