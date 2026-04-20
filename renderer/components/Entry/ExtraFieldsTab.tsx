2f2a6ebcedee91e716aaab70dcd37b312420e574
TinTanSan <tirthvpatel2003@gmail.com>
2026-04-20 15:18:31 +1000

    Refactor: add new extrafield form is now collapsible to make it easier to see all extra fields

--- renderer/components/Entry/ExtraFieldsTab.tsx
+++ renderer/components/Entry/ExtraFieldsTab.tsx
@@ -4,6 +4,7 @@ import { Entry, ExtraField } from '@interfaces/Entry'
 import { BannerContext } from '@contexts/bannerContext'
 import { addBanner } from '@interfaces/Banner'
 import ToggleSwitch from '@components/toggleSwitch'
+import Image from 'next/image'
 
 type props = {
     entry:Entry,
@@ -14,7 +15,8 @@ type props = {
 export default function ExtraFieldsTab({entry, setEntry}:props) {
     const {setBanners} = useContext(BannerContext);
     
-    const [newExtraField, setNewExtraField] = useState<ExtraField>({name:"", data:Buffer.from(""), isProtected:false})
+    const [newExtraField, setNewExtraField] = useState<ExtraField>({name:"", data:Buffer.from(""), isProtected:false});
+    const [showNewExtraFieldForm, setShowNewExtraFieldForm] = useState(false);
 
     const handleDeleteExtraField = (name:string)=>{
         window.entryIPC.removeExtraField(entry.metadata.uuid, name).then((response)=>{
@@ -46,10 +48,8 @@ export default function ExtraFieldsTab({entry, setEntry}:props) {
     }
 
     
-    const handleChange =(e:React.ChangeEvent<HTMLInputElement>)=>{
-        if (e.target.id === "isProtected"){
-            setNewExtraField(prev=>({...prev, isProtected: e.target.checked}))
-        }else if (e.target.id === "data"){
+    const handleChange =(e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
+        if (e.target.id === "data"){
             setNewExtraField(prev=>({...prev, data: Buffer.from(e.target.value)}));
         }else{
             setNewExtraField(prev=>({...prev, name:e.target.value}))
@@ -66,31 +66,32 @@ export default function ExtraFieldsTab({entry, setEntry}:props) {
                 {entry.extraFields.map(ef=><ExtraFieldComponent key={ef.name} extraField={ef} entry={entry} onDelete={handleDeleteExtraField} />)}
             </div>
             {/* add extrafield form */}
-            <div className='flex flex-col w-full h-1/3 grow-0  shrink-0 rounded-lg p-1'>
-                <div className='flex w-full items-center justify-end relative h-10'>
-                    <div className='flex items-center w-full justify-center'>
-                        Add another extra Field
-                    </div>
+            <div className='flex flex-col w-full h-fit justify-center items-center border-2 p-1 border-base-300 rounded-lg text-base-content'>
+                <div className='flex w-full h-10 items-center justify-between  cursor-pointer' onClick={()=>{setShowNewExtraFieldForm(prev=>!prev)}} >
+                    <p className='flex text-subheading'>Add new extra field</p>
+                    <button className='flex w-8 h-8 items-center justify-center' >
+                        <Image src={'/images/up_arrow.svg'} alt='^' width={0} height={0} className={`flex ${showNewExtraFieldForm? "rotate-180": "rotate-0"} h-auto w-8 duration-300 transition-all`}/>
+                    </button>
                 </div>
-                <form onSubmit={handleAddExtraField} className='flex flex-col h-full w-full justify-between'>
-                    <div className='flex gap-2 w-full h-fit'>
+                <form onSubmit={handleAddExtraField} className={`flex flex-col text-normal gap-2 w-full ${showNewExtraFieldForm? "h-[20vh] visible" : "h-0 collapse"} transition-all duration-300`}>
+                    <div className='flex gap-2 w-full h-fit shrink-0'>
                         <label className='flex w-12' >Name</label>
-                        <input type="text" id='name' onChange={handleChange} value={newExtraField.name} className='flex border-2 border-base-300 outline-none focus:border-base-contentb px-1 bg-base-100 rounded-lg w-full' />
+                        <input type="text" id='name' onChange={handleChange} value={newExtraField.name} className='flex border-2 border-base-300 outline-none focus:border-base-contentb px-1 bg-base-100 h-8 rounded-lg w-full' />
+                        <div className='flex w-fit h-full gap-1 items-center text-normal '>
+                            <p className='flex text-nowrap'>Encrypt field?</p>
+                            <ToggleSwitch value={newExtraField.isProtected} setValue={(newVal:boolean)=>{setNewExtraField(prev=>({...prev, isProtected:newVal}))}} />
+                        </div>
                     </div>
-                    <div className='flex gap-2 w-full h-fit'>
+                    <div className='flex w-full h-full'>
                         <label className='flex w-12' >Data</label>
-                        <input type="text" id="data"  onChange={handleChange} value={newExtraField.data.toString()} className='flex border-2 border-base-300 outline-none focus:border-base-content px-1 bg-base-100 rounded-lg w-full' />
+                        <textarea id="data"  onChange={handleChange} value={newExtraField.data.toString()} className='flex border-2 border-base-300 resize-none outline-none focus:border-base-content px-1 bg-base-100 rounded-lg w-full h-full' />
                     </div>
-                    <div className='flex w-full h-fit gap-2 items-center'>
-                        <p>Encrypt field?</p>
-                        {/* <input type="checkbox" id="isProtected" onChange={handleChange} checked={newExtraField.isProtected} className='flex border-2 rounded-full w-4 h-full' />
-                         */}
-                        <ToggleSwitch value={newExtraField.isProtected} setValue={(newVal:boolean)=>{setNewExtraField(prev=>({...prev, isProtected:newVal}))}} />
-                    </div>
-                    <button type="submit" className='flex w-full h-10 items-center justify-center border-2 bg-primary hover:bg-primary-darken text-primary-content hover:rounded-xl transition-all duration-300 rounded-lg'>Add</button>
-                
+                    
+                    <button type="submit" className='flex w-full h-10 shrink-0 items-center justify-center bg-primary hover:bg-primary-darken text-primary-content hover:rounded-xl transition-all duration-300 rounded-lg'>Add</button>
+
                 </form>
             </div>
+
         </div>
     )
 }
