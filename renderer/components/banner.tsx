ba06d9b1734c93e7d75db86cc7a6a3d0625e0aa9
TinTanSan <tirthvpatel2003@gmail.com>
2026-05-06 14:43:11 +1000

    REFACTOR: removed the  handleColour function in favour of an object, additionally moved the object outside of the component as it will never change.

--- renderer/components/banner.tsx
+++ renderer/components/banner.tsx
@@ -2,30 +2,21 @@ import React, { useContext,} from 'react'
 import { BannerDetails } from '@interfaces/Banner'
 import { BannerContext } from '@contexts/bannerContext';
 
+const colourStyle = {
+    'success':' bg-success/75 border-success-darken text-success-content',
+    'info':' bg-info/75 border-info-darken text-info-content',
+    "warning":' bg-warning/75 border-warning-darken text-warning-content',
+    "error":' bg-error/75 border-error-darken text-error-content'
+}
 
 export default function Banner({bannerDetails}:{bannerDetails:BannerDetails}) {
     const banners = useContext(BannerContext);
-    const handleColour = ()=>{
-        if (bannerDetails.type === 'success'){
-            return ' bg-success/75 border-success-darken text-success-content'
-        }else if (bannerDetails.type === "info"){
-            return ' bg-info/75 border-info-darken text-info-content'
-        }else if (bannerDetails.type === 'warning'){
-            return ' bg-warning/75 border-warning-darken text-warning-content'
-        }else{
-            return ' bg-error/75 border-error-darken text-error-content'
-        }
-    }
-
-    
-
-
     
   return (
     <div className={`flex flex-row relative animate-slide-in  border-r-2 border-t-2 border-b-2 border-base-300 gap-1 justify-between items-center overflow-hidden rounded-lg h-14 max-h-fit bg-white w-full`}>
-        <div className={`flex w-2 h-full ${handleColour()}`} />
-        <div className='flex w-full h-full items-center py-2'>{ bannerDetails.text }</div>
-        <button className='flex text-2xl w-fit h-fit' onClick={()=>{banners.setBanners(prev=>prev.filter(x=>x.id !== bannerDetails.id))}}>&#x2716;</button>
+        <div className={`flex w-1 h-full ${colourStyle[bannerDetails.type]}`} />
+        <div className='flex w-full h-full items-center py-2 overflow-hidden text-ellipsis'>{ bannerDetails.text }</div>
+        <button className='flex text-2xl w-fit h-fit font-light' onClick={()=>{banners.setBanners(prev=>prev.filter(x=>x.id !== bannerDetails.id))}}>&#x2716;</button>
     </div>
   )
 }
