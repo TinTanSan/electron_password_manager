ca47ba5d7ac4480d717f8dafebebd5b2619f2346
TinTanSan <tirthvpatel2003@gmail.com>
2026-05-06 14:44:26 +1000

    DELETE: Deleted unused file

--- renderer/interfaces/utils.ts
+++
@@ -1,6 +0,0 @@
-import { PreferenceType } from "@contexts/preferencesContext";
-
-export type BooleanPreferenceKeys = {
-    [K in keyof PreferenceType]: PreferenceType[K] extends boolean ? K : never
-
-}
