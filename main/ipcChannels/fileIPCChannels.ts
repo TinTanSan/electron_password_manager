76be40c457daab0b2a2f940160a8ee57a83b11ae
TinTanSan <tirthvpatel2003@gmail.com>
2026-06-19 06:49:05 +1000

    ADD: added IPC channels for getting files in a directory and getting the home directory

--- main/ipcChannels/fileIPCChannels.ts
+++ main/ipcChannels/fileIPCChannels.ts
@@ -3,6 +3,8 @@ import { ipcRenderer } from 'electron';
 export const fileIPCChannels = {
   openFilePicker: () => ipcRenderer.invoke('fileDialog:open'),
   openCreateFile: () => ipcRenderer.invoke('fileDialog:create'),
+  getHomeDir: ()=>ipcRenderer.invoke('file:homeDir'),
+  getFiles: (filePath:string)=>ipcRenderer.invoke('file:get', filePath),
   deleteFile: (filepath: string) => ipcRenderer.invoke('file:delete', filepath),
   getRecents: () => ipcRenderer.invoke('recents:get'),
   removeRecent: (filePath: string) => ipcRenderer.invoke('recents:remove', filePath),
